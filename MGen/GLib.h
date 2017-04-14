e93ad9c243ff495da9b065559dac72bfd050f015
Alexey Arkhipenko <rualark@gmail.com>
2017-04-14 22:29:57 +0300

    Fix flat note names conversion

--- MGen/GLib.h
+++ MGen/GLib.h
@@ -63,16 +63,16 @@ const CString NoteName[] = {
 
 const CString NoteName2[] = {
 	"C", // 0
-	"DB", // 1
+	"Db", // 1
 	"D", // 2
-	"EB", // 3
+	"Eb", // 3
 	"E", // 4
 	"F", // 5
-	"GB", // 6
+	"Gb", // 6
 	"G", // 7
-	"AB", // 8
+	"Ab", // 8
 	"A", // 9
-	"BB", // 10
+	"Bb", // 10
 	"B" // 11
 };
 
