617ce8891829701c548cd647f3e0a5728fef72f5
Alexey Arkhipenko <rualark@gmail.com>
2017-04-23 14:13:54 +0300

    Create new structure of instrument configurations

---
+++ MGen/instruments/Piano/Default.pl
@@ -0,0 +1,22 @@
+# Main
+Library = Native Instruments Gentleman # For which library algorithm is optimized
+Channel = 0 # MIDI channel
+Type = 0 # Instrument type
+
+# Instrument parameters
+n_min = 21 # Lowest note
+n_max = 108 # Highest note
+t_min = 20 # Shortest note in ms
+t_max = 0 # Longest melody withot pauses in ms (0 = no limit). Decreases with dynamics
+poly = 100 # Maximum number of simultaneous voices
+leap_t_min = 1 # Shortest note after leap
+#dying = 5 # Seconds to fade out at mf
+lengroup2 = 30 # Percent of grouping notes into groups of 2
+lengroup3 = 30 # Percent of grouping notes into groups of 3
+lengroup4 = 30 # Percent of grouping notes into groups of 4
+lengroup_edt1 = 70 # delta to alter note ending time of all notes in group except for the last (ms)
+lengroup_edt2 = -100 # delta to alter note ending time of last note in group (ms)
+rand_start = 8 # Randomize note starts not greater than percent of note length 
+rand_end = 8 # Randomize note end not greater than percent of note length 
+rand_start_max = 70 # Maximum shift in ms
+rand_end_max = 70 # Maximum shift in ms
--- MGen/instruments/Violin/Friedlander.pl
+++ MGen/instruments/Violin/Friedlander.pl
@@ -1,33 +1,9 @@
-# This file contains parameters of all supported instruments for MIDI port sending
-# Instrument types:
-# 0 - Melodic percussion
-# 1 - Friedlander violin
-Instruments = Piano,Violin
-
-Instrument = Piano
-Channel = 0 # MIDI channel
-Type = 0 # Instrument type
-n_min = 21 # Lowest note
-n_max = 108 # Highest note
-t_min = 20 # Shortest note in ms
-t_max = 0 # Longest melody withot pauses in ms (0 = no limit). Decreases with dynamics
-poly = 100 # Maximum number of simultaneous voices
-leap_t_min = 1 # Shortest note after leap
-#dying = 5 # Seconds to fade out at mf
-library = Native Instruments Gentleman # For which library algorithm is optimized
-lengroup2 = 30 # Percent of grouping notes into groups of 2
-lengroup3 = 30 # Percent of grouping notes into groups of 3
-lengroup4 = 30 # Percent of grouping notes into groups of 4
-lengroup_edt1 = 70 # delta to alter note ending time of all notes in group except for the last (ms)
-lengroup_edt2 = -100 # delta to alter note ending time of last note in group (ms)
-rand_start = 8 # Randomize note starts not greater than percent of note length 
-rand_end = 8 # Randomize note end not greater than percent of note length 
-rand_start_max = 70 # Maximum shift in ms
-rand_end_max = 70 # Maximum shift in ms
-
+# Main
 Instrument = Violin
 Channel = 1 # MIDI channel
 Type = 1 # Instrument type
+
+# Instrument parameters
 n_min = G3 # Lowest note
 n_max = F7 # Highest note
 t_min = 160 # Shortest note in ms
