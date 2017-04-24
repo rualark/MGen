4c9c255f6f0da19278512ada44fd25fda6ff71df
Alexey Arkhipenko <rualark@gmail.com>
2017-04-24 19:50:39 +0300

    Adapt: Structure instrument configurations

--- MGen/instruments/Piano/Default.pl
+++ MGen/instruments/Piano/Default.pl
@@ -10,12 +10,16 @@ t_max = 0 # Longest melody withot pauses in ms (0 = no limit). Decreases with dy
 poly = 100 # Maximum number of simultaneous voices
 leap_t_min = 1 # Shortest note after leap
 #dying = 5 # Seconds to fade out at mf
+
+# Lengroup adaptor
 lengroup2 = 30 # Percent of grouping notes into groups of 2
 lengroup3 = 30 # Percent of grouping notes into groups of 3
 lengroup4 = 30 # Percent of grouping notes into groups of 4
 lengroup_edt1 = 70 # delta to alter note ending time of all notes in group except for the last (ms)
 lengroup_edt2 = -100 # delta to alter note ending time of last note in group (ms)
+
+# Randomization
 rand_start = 8 # Randomize note starts not greater than percent of note length 
-rand_end = 8 # Randomize note end not greater than percent of note length 
 rand_start_max = 70 # Maximum shift in ms
+rand_end = 8 # Randomize note end not greater than percent of note length 
 rand_end_max = 70 # Maximum shift in ms
--- MGen/instruments/Violin/Friedlander.pl
+++ MGen/instruments/Violin/Friedlander.pl
@@ -1,5 +1,5 @@
 # Main
-Instrument = Violin
+library = Friedlander Violin 1.5 # For which library algorithm is optimized
 Type = 1 # Instrument type
 
 # Instrument parameters
@@ -11,35 +11,50 @@ poly = 1 # Maximum number of simultaneous voices
 leap_t_min = 1 # Shortest note after leap
 
 # Automation parameters
-library = Friedlander Violin 1.5 # For which library algorithm is optimized
 CC_dynamics = 11
 CC_dyn_ma = 1 # If we should use moving average to smooth CC dynamics curve before sending
+
+# Retrigger adaptor
 CC_retrigger = 64 # This CC will be set before note retriger and reset soon after
 retrigger_freq = 50 # Percent to use CC retrigger instead of non-legato when repeating notes
 retrigger_min_len = 600 # Minimum next note length in ms to use retrigger
 retrigger_rand_max = 300 # Maximum length in ms to move note end to the left in case of nonlegato retrigger
 retrigger_rand_end = 30 # Maximum percent of note length to move note end to the left in case of nonlegato retrigger
+
+# Slur adaptor
 max_slur_count = 2 # Use slur for 2nd moves, but no longer than X moves
 max_slur_interval = 2 # in semitones
 slur_ks = 44 # Keyswitch to send to create slur
+
+# Legato adaptor
 legato_ahead = 165 # Time in ms to stretch legato notes back to cope with legato delay
 max_ahead_note = 12 # Maximum chromatic interval having ahead property
+
+# Nonlegato adaptor
 nonlegato_freq = 20 # Frequency (in percent) when legato can be replaced with non-legato by moving note end to the left
 nonlegato_minlen = 400 # Minimum note length (in ms) allowed to convert to nonlegato
+
+# Technical parameters
 CC_steps = 9 # Number of CC steps in one note step (please use only odd numbers)
+
+# Transition types
 vel_harsh = 118 # Velocity equal or above this value triggers harsh sustain
 vel_immediate = 84 # Velocity equal or above this value triggers immediate sustain
 vel_normal = 50 # Velocity equal or above this value triggers normal sustain
 vel_gliss = 18 # Velocity below this value triggers glissando transition
 vel_normal_minlen = 600 # Minimum note length that can have a normal or lower sustain
+
+# Gliss adaptor
 gliss_minlen = 1000 # Minimum note length that can have a gliss transition
 gliss_freq = 50 # Frequency of gliss articulation in percent
+
+# Bell adaptor
 bell_start_mul = 0.2 # Multiply dynamics by this parameter at bell start
 bell_end_mul = 0.2 # Multiply dynamics by this parameter at bell end
 bell_start_len = 30 # Percent of notelength to use for slope at bell start
 bell_end_len = 30 # Percent of notelength to use for slope at bell end
 
-
+# This is not working yet
 CC_vib_intens = 1
 CC_vib_speed = 14
 dying = 0 # Seconds to fade out at mf
