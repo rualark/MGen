b2f5d70839ec93f795ab6e8a4684e7ae85824e57
Rualark <rualark@gmail.com>
2017-05-08 13:54:07 +0300

    CF1/CA1: Moved Unfinished fill from id 72 to 33. Reordered flags in configuration.

---
+++ MGen/configs/GenCF1/include/hmean-high.pl
@@ -0,0 +1,4 @@
+# Cantus firmus: higher voice harmonic meaning
+
+harm_var = TS,DS,DST,S,DT,TS,DT # Variants of possible harmonic meaning of each note
+harm_const = TS,DS,T,S,DT,S,D # Constant harmonic meaning that is always possible for particular note (must be after harm_var)
---
+++ MGen/configs/GenCF1/include/hmean-low.pl
@@ -0,0 +1,4 @@
+# Cantus firmus: lower voice harmonic meaning
+
+harm_var = TS,DS,DT,S,DT,TS,D # Variants of possible harmonic meaning of each note
+harm_const = T,DS,T,S,D,S,D # Constant harmonic meaning that is always possible for particular note (must be after harm_var)
