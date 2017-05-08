35cc49462d9b2d0f00607392b28c2029ff5017b3
Rualark <rualark@gmail.com>
2017-05-08 10:02:47 +0300

    CF1: Added Unfinished fill flag

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -10,4 +10,4 @@ playback_speed = 100 # Playback speed in percent
 # The following settings cannot be changed in GUI. You can change them only in this file
 View_timer = 100 # ms between each screen update during generation and playback. 100 ms is recommended. Increase for slower computers
 Step_dyn = 1 # Show dynamics with note opacity for each step of note. Disable for slower computers.
-Debug_level = 2 # Increase to show more debug logs
+Debug_level = 1 # Increase to show more debug logs
