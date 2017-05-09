697ac5c32eac228e768c8df7f0a2ddaf07414cf1
Rualark <rualark@gmail.com>
2017-05-09 11:52:06 +0300

    CA1: Collapsed key detection into single function GetCantusKey2. Extracted CA1 configurations into include files.

---
+++ MGen/configs/GenCA1/include/default.pl
@@ -0,0 +1,22 @@
+include "../../GenCF1/include/default.pl"
+
+# Analysis
+corrections = 1 # Number of corrections to show. Set to 0 to disable corrections
+pre_bad = 6 # How many notes to recalculate before rule violation
+post_bad = 8 # How many notes to recalculate after rule violation
+step_penalty = 3 # Penalty for adding one more changing step while correcting cantus
+pitch_penalty = 1 # Penalty for changing note one more diatonic step while correcting cantus
+show_transpose = 0,12 # Semitones to transpose each voice (separated with comma)
+fullscan_max = 5 # Maximum steps length to full scan. If melody is longer, use SWA
+approximations = 10 # Maximum number of approximations to run if penalty decreases
+swa_steps = 6 # Size of Sliding Window Approximation algorithm window in steps
+correct_range = 4 # Maximum interval allowed between each source and corrected note
+random_key = 0 # Select any of possible keys regardless of last note
+
+# Main parameters
+s_len = 6 # Maximum number of measures to full scan. 6-7 is recommended. Lower values can create less melodic results. Higher values are slow to compute
+random_seed = 0 # Seed melody with random numbers. This ensures giving different results if generation is very slow.
+shuffle = 0 # If you want to shuffle all canti after generation (can shuffle up to 32000 canti)
+
+# Technical parameters
+midifile_in_mul = 1 # Multiply note length with this value when loading
