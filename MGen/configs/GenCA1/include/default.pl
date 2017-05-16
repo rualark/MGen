d46fa56edcf40188bc8309b42148f27ca22e347e
Rualark <rualark@gmail.com>
2017-05-16 21:29:13 +0300

    CA1: Show clib size in logs

--- MGen/configs/GenCA1/include/default.pl
+++ MGen/configs/GenCA1/include/default.pl
@@ -7,15 +7,16 @@ post_bad = 8 # How many notes to recalculate after rule violation
 step_penalty = 3 # Penalty for adding one more changing step while correcting cantus
 pitch_penalty = 1 # Penalty for changing note one more diatonic step while correcting cantus
 show_transpose = 0,12 # Semitones to transpose each voice (separated with comma)
-fullscan_max = 1 # Maximum steps length to full scan. If melody is longer, use SWA
+fullscan_max = 100 # Maximum steps length to full scan. If melody is longer, use SWA
 approximations = 50 # Maximum number of approximations to run if rpenalty decreases
 swa_steps = 6 # Size of Sliding Window Approximation algorithm window in steps
-correct_range = 4 # Maximum interval allowed between each source and corrected note
+correct_range = 7 # Maximum interval allowed between each source and corrected note
 random_key = 0 # Select any of possible keys regardless of last note
 
 # Main parameters
 s_len = 2 # Maximum number of measures to full scan. Higher values are slow to compute
 random_seed = 0 # Seed melody with random numbers. This ensures giving different results if generation is very slow.
+random_range = 0 # Limit scanning to one of possible fast-scan ranges (automatically enabled for RSWA)
 shuffle = 0 # If you want to shuffle all canti after generation (can shuffle up to 32000 canti)
 
 # Technical parameters
