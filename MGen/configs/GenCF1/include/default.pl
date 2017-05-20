541a058c220842ee63dc53f1fcfae9af7bbced01
Rualark <rualark@gmail.com>
2017-05-20 16:02:32 +0300

    CF1: Add absolute constant harmonic meaning of notes: harm_const2 (hconst)

--- MGen/configs/GenCF1/include/default.pl
+++ MGen/configs/GenCF1/include/default.pl
@@ -4,6 +4,8 @@
 
 # Method parameters
 # Recommended: method=window-scan, accept_reseed=1, random_seed=1, late_require=1, s_len=1, random_range=0
+# Full scan: method=window-scan, accept_reseed=0, random_seed=0, s_len>c_len-3
+
 method = window-scan # This method is fastest random generator with s_len=1, accept_reseed=1, random_seed=1
 #method = swa # This method is mainly used for testing (same algorithm used as in CA1)
 
