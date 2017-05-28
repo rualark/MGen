4bfdd87dd2a405620534deeb847f36661cf11356
Rualark <rualark@gmail.com>
2017-05-28 17:04:17 +0300

    CP1: Add inside unison and unison direct exit rules. Show cantus id.

--- MGen/configs/GenCP1/include/CP1.pl
+++ MGen/configs/GenCP1/include/CP1.pl
@@ -10,7 +10,7 @@ accept_reseed = 0 # After accepting first result reseed (if random_seed) and cho
 random_seed = 1 # Seed melody with random numbers. This ensures giving different results if generation is very slow.
 random_range = 0 # Limit scanning to one of possible fast-scan ranges (automatically enabled for RSWA)
 s_len = 1 # Maximum number of measures to full scan. Higher values are slow to compute
-cantus_id = 4 # Select particular cantus id. Set to 0 to select random
+cantus_id = 0 # Select particular cantus id. Set to 0 to select random
 
 shuffle = 0 # If you want to shuffle all canti after generation (can shuffle up to 32000 canti)
 
@@ -18,7 +18,7 @@ shuffle = 0 # If you want to shuffle all canti after generation (can shuffle up 
 random_key = 0 # Select any of possible keys regardless of last note
 
 # Main parameters
-interval = 2-9 # Minimum-maximum diatonic interval in counterpoint (7 = octave)
+interval = 0-9 # Minimum-maximum diatonic interval in counterpoint (7 = octave)
 cantus_high = 0 # Set to 1 to generate counterpoint under cantus
 
 # Technical parameters
