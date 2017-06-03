0370904cd6137f06942fea9d5076f760218f6e98
Rualark <rualark@gmail.com>
2017-06-03 21:07:56 +0300

    CP1: Separate rule "Imperfect consonances sequence" into 3 and >3

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -3,5 +3,5 @@ include "../GenCF1/include/hmean-low.pl"
 
 # Analysis
 #Midi_file = "load\test-load-2c1-v2.mid" # Path and file name of imported midi file
-Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
-#Midi_file = "load\cpointer-2c1-2.mid" # Path and file name of imported midi file
+#Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
+Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
