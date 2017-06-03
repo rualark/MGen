5f6fa096b884ed99ef483d218fbf44c70bb8e5cf
Rualark <rualark@gmail.com>
2017-06-03 20:37:51 +0300

    Fixed Project name not visible in title bar in Windows 10

---
+++ MGen/MyVisualManagerOffice2007.cpp
@@ -0,0 +1,25 @@
+// MyVisualManagerOffice2007.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "MGen.h"
+#include "MyVisualManagerOffice2007.h"
+
+IMPLEMENT_DYNCREATE(CMyVisualManagerOffice2007, CMFCVisualManagerOffice2007);
+
+// CMyVisualManagerOffice2007
+
+CMyVisualManagerOffice2007::CMyVisualManagerOffice2007()
+{
+}
+
+CMyVisualManagerOffice2007::~CMyVisualManagerOffice2007()
+{
+}
+
+BOOL CMyVisualManagerOffice2007::DrawTextOnGlass(CDC * pDC, CString strText, CRect rect, DWORD dwFlags, int nGlowSize, COLORREF clrText)
+{
+	return CMFCVisualManagerOffice2007::DrawTextOnGlass(pDC, strText, rect, dwFlags, nGlowSize, RGB(0, 0, 0));
+}
+
+// CMyVisualManagerOffice2007 member functions
