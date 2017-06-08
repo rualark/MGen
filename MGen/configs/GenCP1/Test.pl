2f298e454fe43e70edca96d68db2c334d9e97db7
Rualark <rualark@gmail.com>
2017-06-08 22:24:23 +0300

    CP1: Change flags vector size to real used in configuration. Fixed Generation is not working when calculate_blocking = 0

--- MGen/configs/GenCP1/Test.pl
+++ MGen/configs/GenCP1/Test.pl
@@ -2,5 +2,5 @@ include "include/CP1.pl"
 include "../GenCF1/include/hmean-low.pl"
 
 # Analysis
-#Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
-Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
+Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
+#Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
