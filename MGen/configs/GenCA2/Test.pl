90eab753231339aec9bb36b928c86706c21ea1d7
Rualark <rualark@gmail.com>
2017-06-11 00:27:46 +0300

    CP1: LoadCP: Load lyrics from file

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -2,6 +2,6 @@ include "include/CA2.pl"
 include "../GenCF1/include/hmean-low.pl"
 
 # Analysis
-Midi_file = "load\test-2c1.mid" # Path and file name of imported midi file
-#Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
+#Midi_file = "load\test-2c1.mid" # Path and file name of imported midi file
+Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
 #Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
