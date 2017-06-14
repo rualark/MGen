9165becc3397b214f289a676b634ba6427edded7
Rualark <rualark@gmail.com>
2017-06-14 23:33:12 +0300

    Cf1: Granulate minor unaltered near altered into close and far

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -3,4 +3,5 @@ include "../GenCF1/include/hmean-low.pl"
 
 # Analysis
 #Midi_file = load\test-repeat.mid # Path and file name of imported midi file
-Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
+#Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
+Midi_file = load\test-minor-alter.mid
