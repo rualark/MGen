dfe8fab9e7a654d8ec80731a815c6e0e15b37ed7
Rualark <rualark@gmail.com>
2017-06-17 00:52:00 +0300

    CP1: Generation and visualization now works

---
+++ MGen/configs/GenCP2/Test.pl
@@ -0,0 +1,5 @@
+include "include/CP2.pl"
+
+# Analysis
+Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
+#Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
