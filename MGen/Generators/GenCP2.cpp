79c5335a8f67e971cddb1c1a5725a91a90718153
Rualark <rualark@gmail.com>
2017-06-16 19:24:06 +0300

    CP2: Add new generator

---
+++ MGen/Generators/GenCP2.cpp
@@ -0,0 +1,18 @@
+#include "../stdafx.h"
+#include "GenCP2.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CGenCP2::CGenCP2() {
+}
+
+CGenCP2::~CGenCP2() {
+}
+
+void CGenCP2::LoadConfigLine(CString * sN, CString * sV, int idata, float fdata) {
+}
+
+void CGenCP2::Generate() {
+}
--- MGen/MainFrm.h
+++ MGen/MainFrm.h
@@ -20,6 +20,7 @@
 #include "Generators/GenMP1.h"
 #include "Generators/GenCA1.h"
 #include "Generators/GenCP1.h"
+#include "Generators/GenCP2.h"
 #include "Generators/GenRL1.h"
 
 #define MIN_HZOOM 1
