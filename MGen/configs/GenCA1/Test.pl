c74934b4b8d7e75b8f9b7af5e414eab926205654
Rualark <rualark@gmail.com>
2017-06-27 23:05:18 +0300

    CF1: Show bad hm

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -2,6 +2,6 @@ include "include/CA1.pl"
 
 # Analysis
 #Midi_file = load\test-repeat.mid # Path and file name of imported midi file
-#Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
+Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
 #Midi_file = load\test-minor-alter.mid
-Midi_file = load\test-hsp.mid
+#Midi_file = load\test-hsp.mid
