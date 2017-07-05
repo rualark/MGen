6c03f3b9deef741c8049f8a1395d867ba3f5934a
Rualark <rualark@gmail.com>
2017-07-05 08:35:54 +0300

    CF1: Fix bug searching for first tonic notes before enough notes generated

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -5,6 +5,7 @@ include "include/CA2.pl"
 #Midi_file = "load\test-last-direct.mid" # Path and file name of imported midi file
 #Midi_file = "load\test-tonic-seq.mid" # Path and file name of imported midi file
 #Midi_file = "load\test-overlap.mid" # Path and file name of imported midi file
-Midi_file = "load\test-2c1-2.mid" # Path and file name of imported midi file
+#Midi_file = "load\test-2c1-2.mid" # Path and file name of imported midi file
+Midi_file = "load\test-cp.mid" # Path and file name of imported midi file
 #Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
 #Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
