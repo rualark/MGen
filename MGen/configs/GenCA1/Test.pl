6b844a6bdf5d9ad95fb6598dea23b978aa269d2d
Rualark <rualark@gmail.com>
2017-07-08 10:03:43 +0300

    Intermediate commit

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -1,9 +1,9 @@
 include "include/CA1.pl"
 
 # Analysis
-#Midi_file = load\test-leaps.mid # Path and file name of imported midi file
-Midi_file = load\test-leap-fill.mid # Path and file name of imported midi file
-#Midi_file = load\test-repeat.mid # Path and file name of imported midi file
-#Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
-#Midi_file = load\test-minor-alter.mid
-#Midi_file = load\test-hsp.mid
+#Midi_file = midi\test-leaps.mid # Path and file name of imported midi file
+Midi_file = midi\test-leap-fill.mid # Path and file name of imported midi file
+#Midi_file = midi\test-repeat.mid # Path and file name of imported midi file
+#Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
+#Midi_file = midi\test-minor-alter.mid
+#Midi_file = midi\test-hsp.mid
