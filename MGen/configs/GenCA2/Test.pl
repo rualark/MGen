f4da7c90af80721978de25a61ef0a18ce29d5370
Rualark <rualark@gmail.com>
2017-07-08 10:03:43 +0300

    Intermediate commit

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -1,11 +1,11 @@
 include "include/CA2.pl"
 
 # Analysis
-#Midi_file = "load\test-tonicroot.mid" # Path and file name of imported midi file
-#Midi_file = "load\test-last-direct.mid" # Path and file name of imported midi file
-#Midi_file = "load\test-tonic-seq.mid" # Path and file name of imported midi file
-#Midi_file = "load\test-overlap.mid" # Path and file name of imported midi file
-#Midi_file = "load\test-2c1-2.mid" # Path and file name of imported midi file
-Midi_file = "load\test-cp.mid" # Path and file name of imported midi file
-#Midi_file = "load\known-cp2v1.mid" # Path and file name of imported midi file
-#Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-tonicroot.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-last-direct.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-tonic-seq.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-overlap.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-2c1-2.mid" # Path and file name of imported midi file
+Midi_file = "midi\test-cp.mid" # Path and file name of imported midi file
+#Midi_file = "midi\known-cp2v1.mid" # Path and file name of imported midi file
+#Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
--- MGen/configs/GenCP1/Test.pl
+++ MGen/configs/GenCP1/Test.pl
@@ -1,5 +1,5 @@
 include "include/CP1.pl"
 
 # Analysis
-Midi_file = load\known-cf-maj.mid # Path and file name of imported midi file
-#Midi_file = "load\cpointer-2c1.mid" # Path and file name of imported midi file
+Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
+#Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
