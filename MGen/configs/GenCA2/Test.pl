fcfcb314f105d078c06eca23381624c19a6811e0
Rualark <rualark@gmail.com>
2017-07-12 19:35:22 +0300

    CP1: Add GetNoteTypes

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -6,7 +6,8 @@ include "include/CA2.pl"
 #Midi_file = "midi\test-tonic-seq.mid" # Path and file name of imported midi file
 #Midi_file = "midi\test-overlap.mid" # Path and file name of imported midi file
 #Midi_file = "midi\test-2c1-2.mid" # Path and file name of imported midi file
+Midi_file = "midi\test-2c1.mid" # Path and file name of imported midi file
 #Midi_file = "midi\test-cp.mid" # Path and file name of imported midi file
 #Midi_file = "midi\known-cp2v1.mid" # Path and file name of imported midi file
 #Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
-Midi_file = "midi\test-alterint.mid" # Path and file name of imported midi file
+#Midi_file = "midi\test-alterint.mid" # Path and file name of imported midi file
