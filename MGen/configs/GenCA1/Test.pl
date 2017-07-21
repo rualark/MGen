82b302fb319f4ba6565a1d5ebe8135815d812d0f
Rualark <rualark@gmail.com>
2017-07-21 09:33:34 +0300

    CF1: Fixed Adjacent repeat is marked at a different step then symmetric repeat (closed #655)

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -2,10 +2,11 @@ include "include/CA1.pl"
 
 # Analysis
 #Midi_file = midi\test-leaps.mid # Path and file name of imported midi file
-Midi_file = midi\test-leap-fill.mid # Path and file name of imported midi file
+#Midi_file = midi\test-leap-fill.mid # Path and file name of imported midi file
 #Midi_file = "midi\test-short-leap.mid" # Path and file name of imported midi file
 #Midi_file = midi\test-repeat.mid # Path and file name of imported midi file
 #Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
+Midi_file = midi\known-cf-min.mid # Path and file name of imported midi file
 #Midi_file = midi\test-minor-alter.mid
 #Midi_file = midi\test-tritone.mid
 #Midi_file = midi\test-hsp.mid
