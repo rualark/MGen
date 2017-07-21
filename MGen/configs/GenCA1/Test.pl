10719efff7a7f3cbe0c19ee6494c53c6e65e0c21
Rualark <rualark@gmail.com>
2017-07-21 10:02:09 +0300

    CF1: Small additions to midi file

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -6,8 +6,8 @@ include "include/CA1.pl"
 #Midi_file = "midi\GenCA1\test-short-leap.mid" # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-repeat.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\known-cf-maj.mid # Path and file name of imported midi file
-Midi_file = midi\GenCA1\known-cf-min.mid # Path and file name of imported midi file
-#Midi_file = midi\GenCA1\test-minor-alter.mid
+#Midi_file = midi\GenCA1\known-cf-min.mid # Path and file name of imported midi file
+Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
 #Midi_file = midi\GenCA1\test-hsp.mid
 #Midi_file = midi\GenCA1\test-leap-size.mid
