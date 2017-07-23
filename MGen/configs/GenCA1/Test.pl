9c9fb172c120b42a948b34eff0826a285ec63914
Rualark <rualark@gmail.com>
2017-07-23 22:56:24 +0300

    CF1: Fixed Minimum range rule not working

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -1,7 +1,7 @@
 include "include/CA1.pl"
 
 # Analysis
-Midi_file = midi\GenCA1\test-leaps.mid # Path and file name of imported midi file
+#Midi_file = midi\GenCA1\test-leaps.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-leap-fill.mid # Path and file name of imported midi file
 #Midi_file = "midi\GenCA1\test-short-leap.mid" # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-repeat.mid # Path and file name of imported midi file
@@ -11,3 +11,4 @@ Midi_file = midi\GenCA1\test-leaps.mid # Path and file name of imported midi fil
 #Midi_file = midi\GenCA1\test-tritone.mid
 #Midi_file = midi\GenCA1\test-hsp.mid
 #Midi_file = midi\GenCA1\test-leap-size.mid
+Midi_file = "midi\result\2017-07-22-auto 18-42-47.mid"
