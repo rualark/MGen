56fd9000908c54d0271e90fbd582c1dde36a5dcf
Rualark <rualark@gmail.com>
2017-07-26 20:31:49 +0300

    CF1: Parse repeat rules parameters from rules.xlsm

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -4,10 +4,10 @@ include "include/CA1.pl"
 #Midi_file = midi\GenCA1\test-leaps.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-leap-fill.mid # Path and file name of imported midi file
 #Midi_file = "midi\GenCA1\test-short-leap.mid" # Path and file name of imported midi file
-#Midi_file = midi\GenCA1\test-repeat.mid # Path and file name of imported midi file
+Midi_file = midi\GenCA1\test-repeat.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\known-cf-maj.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\known-cf-min.mid # Path and file name of imported midi file
-Midi_file = midi\GenCA1\test-minor-alter.mid
+#Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
 #Midi_file = midi\GenCA1\test-hsp.mid
 #Midi_file = midi\GenCA1\test-fill.mid
