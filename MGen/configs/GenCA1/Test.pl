032f75148a74bbab8a84fe17335b96a2b5722156
Rualark <rualark@gmail.com>
2017-07-26 10:17:40 +0300

    CF1: Tested Big leaps and culminations should not have bad harmonic transitions (closed #662)

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -9,9 +9,9 @@ include "include/CA1.pl"
 #Midi_file = midi\GenCA1\known-cf-min.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
-#Midi_file = midi\GenCA1\test-hsp.mid
+Midi_file = midi\GenCA1\test-hsp.mid
 #Midi_file = midi\GenCA1\test-fill.mid
-Midi_file = midi\GenCA1\test-culm.mid
+#Midi_file = midi\GenCA1\test-culm.mid
 #Midi_file = midi\GenCA1\test-leapmid.mid
 #Midi_file = midi\GenCA1\test-leap-size.mid
 #Midi_file = midi\GenCA1\test-lrange.mid
