a60e62d3ff8688ebcfc5e90725e404d616210629
Rualark <rualark@gmail.com>
2017-07-30 19:50:42 +0300

    CF1: Fixed local range and ccma bugs

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -6,7 +6,7 @@ include "include/CA1.pl"
 #Midi_file = "midi\GenCA1\test-short-leap.mid" # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-repeat.mid # Path and file name of imported midi file
 #Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
-#Midi_file = midi\GenCA1\known-cf-min.mid # Path and file name of imported midi file
+Midi_file = midi\known-cf-min.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
 #Midi_file = "midi\GenCA1\test-last.mid"
@@ -16,5 +16,5 @@ include "include/CA1.pl"
 #Midi_file = midi\GenCA1\test-culm.mid
 #Midi_file = midi\GenCA1\test-leapmid.mid
 #Midi_file = midi\GenCA1\test-leap-size.mid
-Midi_file = midi\GenCA1\test-lrange.mid
+#Midi_file = midi\GenCA1\test-lrange.mid
 #Midi_file = "midi\result\2017-07-22-auto 18-42-47.mid"
