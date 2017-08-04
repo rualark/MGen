883b975d2b4b368227eeb55e2d55ad2fcec00cc4
Rualark <rualark@gmail.com>
2017-08-04 09:12:20 +0300

    CP1: Decreased quantity of SWA logs

--- MGen/configs/GenCP1/include/CP1.pl
+++ MGen/configs/GenCP1/include/CP1.pl
@@ -20,7 +20,7 @@ shuffle = 0 # If you want to shuffle all canti after generation (can shuffle up 
 random_key = 0 # Select any of possible keys regardless of last note
 
 # Main parameters
-notes_per_measure = 4 # Number of notes per measure (1 = species1, 2 = species2, 4 = species3, 8 = species5)
+notes_per_measure = 1 # Number of notes per measure (1 = species1, 2 = species2, 4 = species3, 8 = species5)
 start_pause = 1 # Length of starting pause at counterpoint voice start (length of pause is controlled by notes_per_measure)
 rpenalty_accepted = 0 # Maximum accepted rule penalty for RandomSWA
 
