c683b5eaf36601791d538073006e31d0b0118017
Rualark <rualark@gmail.com>
2017-08-05 13:19:19 +0300

    Moved Autotest to MGen folder

--- MGen/Autotest/test.bat
+++ MGen/Autotest/test.bat
@@ -1,8 +1,7 @@
 appveyor AddMessage Test started
 
 cd ..
-copy "x64\Release\MGen.exe" "MGen"
-cd "MGen"
+copy "..\x64\Release\MGen.exe" .
 MGen.exe -test configs\GenCA1\Test.pl
 
 appveyor AddMessage Test finished
