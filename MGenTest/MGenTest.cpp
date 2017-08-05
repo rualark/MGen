9e8a47488cd0424ac14b1a98d257cd5570fea0dd
Rualark <rualark@gmail.com>
2017-08-05 15:12:44 +0300

    Added MGenTest empty application

---
+++ MGenTest/MGenTest.cpp
@@ -0,0 +1,50 @@
+// MGenTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "MGenTest.h"
+#include "../MGen/GLibrary/GLib.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int test() {
+	return 0;
+}
+
+int main()
+{
+    int nRetCode = 0;
+
+    HMODULE hModule = ::GetModuleHandle(nullptr);
+
+    if (hModule != nullptr)
+    {
+        // initialize MFC and print and error on failure
+        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+        {
+            // TODO: change error code to suit your needs
+            wprintf(L"Fatal Error: MFC initialization failed\n");
+            nRetCode = 1;
+        }
+        else
+        {
+					test();
+        }
+    }
+    else
+    {
+        // TODO: change error code to suit your needs
+        wprintf(L"Fatal Error: GetModuleHandle failed\n");
+        nRetCode = 1;
+    }
+
+    return nRetCode;
+}
