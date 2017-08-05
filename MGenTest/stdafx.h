8c95b5d0867b61d894782faef759ffdfaecbd2e6
Rualark <rualark@gmail.com>
2017-08-05 15:12:44 +0300

    Added MGenTest empty application

---
+++ MGenTest/stdafx.h
@@ -0,0 +1,49 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // some CString constructors will be explicit
+#define _AFX_NO_MFC_CONTROLS_IN_DIALOGS         // remove support for MFC controls in dialogs
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // Exclude rarely-used stuff from Windows headers
+#endif
+
+#include <afx.h>
+#include <afxwin.h>         // MFC core and standard components
+#include <afxext.h>         // MFC extensions
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // MFC support for Internet Explorer 4 Common Controls
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>                     // MFC support for Windows Common Controls
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <set>
+#include <queue>
+#include <mutex>
+#include <numeric>
+#include <initializer_list>
+#include <random>
+#include <algorithm>
+#include <fstream>
+#include <regex>
+using namespace std;
+#include <chrono>
+using namespace std::chrono;
+#include <gdiplus.h>
+using namespace Gdiplus;
+#include "../MGen/dirent.h"
+
+
+// TODO: reference additional headers your program requires here
