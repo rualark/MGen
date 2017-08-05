a2ee1f613aacd4eda714816a93fd66c6b2f6fc3a
Rualark <rualark@gmail.com>
2017-08-05 20:59:43 +0300

    Divide all logs sent to Warning pane into Warning (1) and Error (5) for testing

--- MGen/configs/GenMP1/autoconf-beyond.pl
+++ MGen/configs/GenMP1/autoconf-beyond.pl
@@ -8,5 +8,5 @@ midifile_in_mul = 8 # How much you want to decrease minimum step length
 
 # This config was created from default config file configs\GenMP1.pl
 # Created at 2017-04-21 18-40-11
-Midi_file = load\beyond.mid
+Midi_file = midi\beyond.mid
 Instruments = Piano,Piano,Trombone,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano
