97770d9b0a5703a5021528d651b02405edc72139
Rualark <rualark@gmail.com>
2017-08-05 16:06:17 +0300

    MGenTest: run multiple tests from program

--- MGenTest/MGenTest.cpp
+++ MGenTest/MGenTest.cpp
@@ -15,7 +15,69 @@ CWinApp theApp;
 
 using namespace std;
 
+CString current_dir;
+ofstream logfile;
+
+void Log(CString st) {
+	cout << st;
+	logfile << st;
+	::ShellExecute(GetDesktopWindow(), "open", "appveyor", "AddMessage \"" + st + "\"", NULL, SW_SHOWNORMAL);
+}
+
+void LoadConfig() {
+	ifstream fs;
+	fs.open("autotest\\test.txt");
+	LPDWORD ecode = new DWORD;
+	CString st, st2;
+	char pch[2550];
+	int pos = 0;
+	while (fs.good()) {
+		fs.getline(pch, 2550);
+		st = pch;
+		pos = st.Find("#");
+		if (pos != -1) st = st.Left(pos);
+		st.Trim();
+		if (st.GetLength()) {
+			Log("Starting test config: " + st + "\n");
+			//::ShellExecute(GetDesktopWindow(), "open", "MGen", "-test " + st, NULL, SW_SHOWNORMAL);
+			st2 = "-test " + st;
+			SHELLEXECUTEINFO sei = { 0 };
+			sei.cbSize = sizeof(SHELLEXECUTEINFO);
+			sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+			sei.hwnd = NULL;
+			sei.lpVerb = NULL;
+			sei.lpFile = "MGen.exe";
+			sei.lpParameters = st2;
+			sei.lpDirectory = NULL;
+			sei.nShow = SW_SHOW;
+			sei.hInstApp = NULL;
+			ShellExecuteEx(&sei);
+			if (WaitForSingleObject(sei.hProcess, 10000) == WAIT_TIMEOUT) {
+				Log("Timeout waiting for process\n");
+				return;
+			}
+
+			GetExitCodeProcess(sei.hProcess, ecode);
+			st2.Format("%d", *ecode);
+			Log("Exit code: " + st2 + "\n");
+		}
+	}
+	delete ecode;
+	fs.close();
+}
+
 int test() {
+	logfile.open("autotest\\test.log", ios_base::app);
+
+	TCHAR buffer[MAX_PATH];
+	GetCurrentDirectory(MAX_PATH, buffer);
+	current_dir = string(buffer).c_str();
+	Log("Current dir: " + current_dir + "\n");
+
+	LoadConfig();
+	logfile.close();
+	cout << "Press any key to continue... ";
+	getch();
 	return 0;
 }
 
@@ -37,7 +99,7 @@ int main()
         else
         {
 					test();
-        }
+				}
     }
     else
     {
