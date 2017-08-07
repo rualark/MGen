d3b08198b2870ae79ac09c41cb55ff138557b6d6
Rualark <rualark@gmail.com>
2017-08-07 21:48:36 +0300

    MGenTest: Send errors in separate run

--- MGenTest/MGenTest.cpp
+++ MGenTest/MGenTest.cpp
@@ -152,12 +152,11 @@ void PublishTest(CString tname, int result, int tpassed) {
 	cout << errors;
 
 	if (ci) {
-		//errors.Replace("\n- ", "\\n ");
 		CString cat = "Passed";
 		if (result) cat = "Failed";
 		st.Format("UpdateTest \"%s\" -Framework MSTest -FileName MGen.exe -Duration %d -Outcome %s -ErrorMessage \"%d\"", tname, tpassed, cat, result);
 		Run("appveyor", st, 1000);
-		st.Format("UpdateTest \"%s\" -ErrorStackTrace \"%s\"", tname, errors);
+		st.Format("UpdateTest \"%s\" -Framework MSTest -FileName MGen.exe -Duration %d -Outcome %s -ErrorStackTrace \"%s\"", tname, tpassed, cat, errors);
 		Run("appveyor", st, 1000);
 	}
 
