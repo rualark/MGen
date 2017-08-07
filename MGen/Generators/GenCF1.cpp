df91d06d6b4ead4e263d3377890c6a2b8a2d332e
Rualark <rualark@gmail.com>
2017-08-07 10:23:27 +0300

    CF1: If testing, enable all disabled rules so that expected violations can be confirmed

--- MGen/Generators/GenCF1.cpp
+++ MGen/Generators/GenCF1.cpp
@@ -162,6 +162,8 @@ void CGenCF1::LoadRules(CString fname)
 			SubRuleName[set][rid] = subrule;
 			RuleComment[rid] = ast[8];
 			SubRuleComment[set][rid] = ast[9];
+			// If testing, enable all disabled rules so that expected violations can be confirmed
+			if (m_testing && flag == -1) flag = 1;
 			accepts[set][rid] = flag;
 			severity[rid] = sev;
 		}
