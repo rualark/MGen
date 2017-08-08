a02f05f29fb4af503d6fb9c3712b40cab20170b5
Rualark <rualark@gmail.com>
2017-08-08 21:41:12 +0300

    CF1: Add more examples

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -9,7 +9,7 @@ include "include/CA1.pl"
 #Midi_file = midi\known-cf-min.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
-#Midi_file = midi\GenCA1\test-manyleaps.mid
+Midi_file = midi\GenCA1\test-manyleaps.mid
 #Midi_file = "midi\GenCA1\test-last.mid"
 #Midi_file = midi\GenCA1\test-hsp.mid
 #Midi_file = midi\GenCA1\test-tonic.mid
@@ -18,4 +18,4 @@ include "include/CA1.pl"
 #Midi_file = midi\GenCA1\test-leapmid.mid
 #Midi_file = midi\GenCA1\test-lrange.mid
 #Midi_file = "midi\result\2017-07-22-auto 18-42-47.mid"
-Midi_file = midi\GenCA1\test-leap-size.mid
+#Midi_file = midi\GenCA1\test-leap-size.mid
