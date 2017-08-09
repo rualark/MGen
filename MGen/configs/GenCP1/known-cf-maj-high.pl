453f197dd04145219881336632f2dd21de66d343
Rualark <rualark@gmail.com>
2017-08-09 22:28:21 +0300

    CP1: c_len is greater than cc_len.size() at SendCP() (closed #790)

--- MGen/configs/GenCP1/known-cf-maj-high.pl
+++ MGen/configs/GenCP1/known-cf-maj-high.pl
@@ -7,4 +7,5 @@ cantus_id = 0 # Select particular cantus id. Set to 0 to select random
 
 #method = window-scan # This method can be faster in some cases
 method = swa # This method is best for analysis and corrections in most cases
-
+notes_per_measure = 2 # Number of notes per measure (1 = species1, 2 = species2, 4 = species3, 8 = species5)
+start_pause = 1 # Length of starting pause at counterpoint voice start (length of pause is controlled by notes_per_measure)
--- MGen/configs/GenCP1/known-cf-maj.pl
+++ MGen/configs/GenCP1/known-cf-maj.pl
@@ -3,6 +3,6 @@ include "include/CP1.pl"
 # Analysis
 Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
 
-cantus_id = 0 # Select particular cantus id. Set to 0 to select random
+cantus_id = 12 # Select particular cantus id. Set to 0 to select random
 
 cantus_high = 0 # Set to 1 to consider cantus to be higher voice
