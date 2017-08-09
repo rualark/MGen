2682d5d6c03eabf2a80c9313126dd2b0fb380440
Rualark <rualark@gmail.com>
2017-08-09 09:10:57 +0300

    Intermediate commit

--- MGen/configs/GenCA1/Test.pl
+++ MGen/configs/GenCA1/Test.pl
@@ -9,10 +9,10 @@ include "include/CA1.pl"
 #Midi_file = midi\known-cf-min.mid # Path and file name of imported midi file
 #Midi_file = midi\GenCA1\test-minor-alter.mid
 #Midi_file = midi\GenCA1\test-tritone.mid
-Midi_file = midi\GenCA1\test-manyleaps.mid
+#Midi_file = midi\GenCA1\test-manyleaps.mid
 #Midi_file = "midi\GenCA1\test-last.mid"
 #Midi_file = midi\GenCA1\test-hsp.mid
-#Midi_file = midi\GenCA1\test-tonic.mid
+Midi_file = midi\GenCA1\test-tonic.mid
 #Midi_file = midi\GenCA1\test-fill.mid
 #Midi_file = midi\GenCA1\test-culm.mid
 #Midi_file = midi\GenCA1\test-leapmid.mid
