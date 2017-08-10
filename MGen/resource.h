ce5544472c3a9d5a852b4e3a1bd709d2cb8412c4
Rualark <rualark@gmail.com>
2017-08-10 23:29:09 +0300

    Note scan range is displayed out of piano roll (closed #803)

--- MGen/resource.h
+++ MGen/resource.h
@@ -126,13 +126,15 @@
 #define ID_OINFO9                       32841
 #define ID_BUTTON_SSTATUS2              32842
 #define ID_BUTTON_SHOW_STATUS           32843
+#define ID_CHECK2                       32843
+#define ID_CHECK_NSR                    32843
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        320
-#define _APS_NEXT_COMMAND_VALUE         32843
+#define _APS_NEXT_COMMAND_VALUE         32844
 #define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
