656ae8b1e6a8dd3d0d6b9b712c6a0658b15f5441
Rualark <rualark@gmail.com>
2017-08-11 19:34:41 +0300

    CP1: RSWA generates rpenalty>0 while accept_rpenalty = 0 (closed #815)

--- MGen/configs/GenCP1/known-cf-maj.pl
+++ MGen/configs/GenCP1/known-cf-maj.pl
@@ -3,8 +3,8 @@ include "include/CP1.pl"
 # Analysis
 Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
 
-method = swa # This method is best for analysis and corrections in most cases
+method = swa
 
 cantus_id = 1 # Select particular cantus id. Set to 0 to select random
 
-cantus_high = 0 # Set to 1 to consider cantus to be higher voice
+cantus_high = 1 # Set to 1 to consider cantus to be higher voice
