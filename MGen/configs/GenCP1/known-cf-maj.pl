01f010b6932254f977b4b8ec25f26a34a59b2eba
Rualark <rualark@gmail.com>
2017-08-11 10:02:35 +0300

    CP1: Can display both with different colors (source for cfv and swa for cpv) CF1: Will display only source (closed #808)

--- MGen/configs/GenCP1/known-cf-maj.pl
+++ MGen/configs/GenCP1/known-cf-maj.pl
@@ -3,6 +3,8 @@ include "include/CP1.pl"
 # Analysis
 Midi_file = midi\known-cf-maj.mid # Path and file name of imported midi file
 
+method = swa # This method is best for analysis and corrections in most cases
+
 cantus_id = 1 # Select particular cantus id. Set to 0 to select random
 
 cantus_high = 0 # Set to 1 to consider cantus to be higher voice
