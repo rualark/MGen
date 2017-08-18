ba75b053081d1e12b046f9e83b1f0d346b6a0780
Rualark <rualark@gmail.com>
2017-08-18 22:23:58 +0300

    CA2: Fixed loading last harmony column in LoadCP

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -4,7 +4,7 @@ include "include/CA2.pl"
 #Midi_file = "midi\GenCA2\test-tonicroot.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-last-direct.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-fis-multivoice.mid" # Path and file name of imported midi file
-Midi_file = "midi\GenCA2\test-gis-multivoice.mid" # Path and file name of imported midi file
+#Midi_file = "midi\GenCA2\test-gis-multivoice.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-tonic-seq.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-overlap.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-2c1-2.mid" # Path and file name of imported midi file
@@ -16,3 +16,4 @@ Midi_file = "midi\GenCA2\test-gis-multivoice.mid" # Path and file name of import
 #Midi_file = "midi\GenCA2\cpointer-2c1.mid" # Path and file name of imported midi file
 #Midi_file = "midi\GenCA2\test-alterint.mid" # Path and file name of imported midi file
 #Midi_file = "midi\temp\2017-08-11-auto 23-36-05.mid"
+Midi_file = "midi\GenCA2\test-sus.mid" # Path and file name of imported midi file
