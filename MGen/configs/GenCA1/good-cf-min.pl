8fa448bd3132c40915a6a95e85448a69099b6f56
Rualark <rualark@gmail.com>
2017-08-22 10:13:57 +0300

    Extracted good-cf-min from known-cf-min.mid

---
+++ MGen/configs/GenCA1/good-cf-min.pl
@@ -0,0 +1,5 @@
+include "include/CA1.pl"
+
+Midi_file = midi\good-cf-min.mid # Path and file name of imported midi file
+
+corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
