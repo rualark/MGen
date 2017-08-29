bf05d2bffcc1d5cd4725a7379432192b44dec729
Rualark <rualark@gmail.com>
2017-08-29 20:40:03 +0300

    CP1: Fixed FailSus bug

--- MGen/Generators/GenCP1.cpp
+++ MGen/Generators/GenCP1.cpp
@@ -460,7 +460,7 @@ int CGenCP1::FailVMotion() {
 
 int CGenCP1::FailSus() {
 	int pre_end;
-	for (ls = 0; ls < fli_size; ++ls) if (sus[ls] && (ls < fli_size-1 || ep2 < c_len)) {
+	for (ls = 0; ls < fli_size; ++ls) if (sus[ls] && (ls < fli_size-1 || ep2 == c_len)) {
 		s = fli[ls];
 		s2 = fli2[ls];
 		// Flag suspension
