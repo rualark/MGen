22ccca94eb0891ff6e614b101743d4d5bdcfc3af
Rualark <rualark@gmail.com>
2017-08-31 18:40:02 +0300

    CP3: SWA returns stable Out of range (closed #938)

--- MGen/configs/GenCP1/Test.pl
+++ MGen/configs/GenCP1/Test.pl
@@ -5,6 +5,6 @@ Midi_file = midi\good-cf-min.mid # Path and file name of imported midi file
 #Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
 
 method = swa
-cantus_id = 1
+cantus_id = 14
 cantus_high = 0
-species = 2
+species = 3
