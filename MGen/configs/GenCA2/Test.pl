433c4e46e412539b58de6bd0f5e24a449affca82
Rualark <rualark@gmail.com>
2017-09-08 00:43:08 +0300

    CP1: Add flag "Pause longer than 1/2" for all CP in FailRhythm and FailSlurs

--- MGen/configs/GenCA2/Test.pl
+++ MGen/configs/GenCA2/Test.pl
@@ -24,3 +24,5 @@ include "include/CA2.pl"
 #Midi_file = "midi\temp\temp.mid"
 #Midi_file = "midi\GenCA2\test-pco-apart.mid" # Path and file name of imported midi file
 Midi_file = "midi\GenCA2\test-cp5-rhythm.mid" # Path and file name of imported midi file
+#Midi_file = "midi\GenCA2\test-cp2-rhythm.mid" # Path and file name of imported midi file
+#Midi_file = "midi\GenCA2\test-cp3-rhythm.mid" # Path and file name of imported midi file
