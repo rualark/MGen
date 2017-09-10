2e9934f6377f41114304ceda925f24dbd05ef88c
Rualark <rualark@gmail.com>
2017-09-10 17:24:08 +0300

    CA5: Wrong distance penalty shown when animating in InfoDlg for SWA (closed #1083). Disabled optimize_dpenalty flag for CP1.

--- MGen/configs/GenCA2/include/CA2.pl
+++ MGen/configs/GenCA2/include/CA2.pl
@@ -11,7 +11,7 @@ random_seed = 0 # Seed melody with random numbers. This ensures giving different
 random_range = 0 # Limit scanning to one of possible fast-scan ranges (automatically enabled for RSWA)
 s_len = 1 # Maximum number of measures to full scan. Higher values are slow to compute
 correct_inrange = 2 # Limit allowed range of corrected melody to range of source melody + specified number of chromatic steps. Set to high value to disable.
-optimize_dpenalty = 0 # Saves only melodies closest to source melody. Decreases memory usage. Setting this flag may make corrections farther from source melody. Resetting allows for more close results when corrections>1
+optimize_dpenalty = 1 # Saves only melodies closest to source melody. Decreases memory usage. Setting this flag may make corrections farther from source melody. Resetting allows for closer results when corrections>1
 max_correct_ms = 50000 # Maximum time in milliseconds to correct using window-scan (set to 0 to scan up to the end)
 
 # Analysis
