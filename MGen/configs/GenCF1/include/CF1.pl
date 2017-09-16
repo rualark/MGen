23263b56054ddfa766a07942dd6de4f597624c85
Rualark <rualark@gmail.com>
2017-09-16 21:16:42 +0300

    Fixed local and global false positives

--- MGen/configs/GenCF1/include/CF1.pl
+++ MGen/configs/GenCF1/include/CF1.pl
@@ -58,5 +58,5 @@ midifile_export_comments = 1 # Set to export comments (violations) to midifile l
 
 # Automatic testing
 # Specify rule identiefier, which should not be tested for false positive
-false_positives_ignore = 4,5,10,15,16,36,76,198
+false_positives_ignore = 4,5,10,15,16,36,76,98,198
 false_positives_global = 1,2,6,9,12,13,14,18,19,24,28,30,32,33,34,40,41,49,50,51,52,58,67,68,69,74,75,87,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186
