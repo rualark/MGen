7e5cfd3619ef39a37f9823beb2934768fdb9e542
Rualark <rualark@gmail.com>
2017-09-17 21:16:40 +0300

    CA2: Better confirmation and emulator logs

--- MGen/Generators/GenCA1.cpp
+++ MGen/Generators/GenCA1.cpp
@@ -334,8 +334,9 @@ void CGenCA1::CheckSASEmulatorFlags() {
 			delay = ep2 - 1 - s;
 			if (delay > flag_delay[fl]) {
 				CString est;
-				est.Format("SAS emulator at step %d has delay %d steps: [%d] %s %s (%s) at %d:%d %s",
-					ep2 - 1, delay, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, s + 1, midi_file);
+				est.Format("SAS emulator at step %d has delay %d steps: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+					ep2 - 1, delay, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+					cantus_id + 1, s + 1, (step0 + fn + s + 1) / 8 + 1, (step0 + fn + s + 1) % 8, midi_file);
 				//WriteLog(1, est);
 				flag_delay[fl] = delay;
 				flag_delay_st[fl] = est;
@@ -350,8 +351,9 @@ void CGenCA1::CheckSASEmulatorFlags() {
 				// Not found in same position: does it exist in any position?
 				if (flags_full[fl]) {
 					CString est;
-					est.Format("SAS emulator at step %d assigned moved flag: [%d] %s %s (%s) at %d:%d %s",
-						ep2 - 1, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, s + 1, midi_file);
+					est.Format("SAS emulator at step %d assigned moved flag: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+						ep2 - 1, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+						cantus_id + 1, s + 1, (step0 + fn + s + 1) / 8 + 1, (step0 + fn + s + 1) % 8, midi_file);
 					if (sas_emulator_move_ignore[fl]) {
 						WriteLog(6, est);
 					}
@@ -361,8 +363,9 @@ void CGenCA1::CheckSASEmulatorFlags() {
 				}
 				else {
 					CString est;
-					est.Format("SAS emulator at step %d assigned wrong flag: [%d] %s %s (%s) at %d:%d %s",
-						ep2 - 1, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, s + 1, midi_file);
+					est.Format("SAS emulator at step %d assigned wrong flag: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+						ep2 - 1, fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+						cantus_id + 1, s + 1, (step0 + fn + s + 1) / 8 + 1, (step0 + fn + s + 1) % 8, midi_file);
 					WriteLog(6, est); // 1
 				}
 			}
@@ -404,8 +407,9 @@ void CGenCA1::ConfirmExpect() {
 			}
 			else if (debug_level > 0) {
 				CString est;
-				est.Format("Expected flag confirmed: [%d] %s %s (%s) at %d:%d %s",
-					fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, x + 1, midi_file);
+				est.Format("Expected flag confirmed: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+					fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+					cantus_id + 1, x + 1, (step0 + fn + x + 1) / 8 + 1, (step0 + fn + x + 1) % 8, midi_file);
 				WriteLog(6, est);
 				if (m_testing) AppendLineToFile("autotest\\expect.log", est + "\n");
 			}
@@ -418,8 +422,9 @@ void CGenCA1::ConfirmExpect() {
 			for (int f = 0; f < anflags[cpv][s].size(); ++f) if (fl == anflags[cpv][s][f]) {
 				if (!enflags2[fl][s]) {
 					CString est;
-					est.Format("Local false positive flag: [%d] %s %s (%s) at %d:%d %s",
-						fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, s + 1, midi_file);
+					est.Format("Local false positive flag: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+						fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+						cantus_id + 1, s + 1, (step0 + fn + s + 1) / 8 + 1, (step0 + fn + s + 1) % 8, midi_file);
 					WriteLog(5, est);
 					//if (m_testing) AppendLineToFile("autotest\\expect.log", est + "\n");
 				}
@@ -434,8 +439,9 @@ void CGenCA1::ConfirmExpect() {
 			fl = anflags[cpv][s][f];
 			if (!enflags2[fl][s] && false_positives_global[fl]) {
 				CString est;
-				est.Format("Global false positive flag: [%d] %s %s (%s) at %d:%d %s",
-					fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], cantus_id + 1, s + 1, midi_file);
+				est.Format("Global false positive flag: [%d] %s %s (%s) at %d:%d (beat %d:%d) %s",
+					fl, accept[fl] ? "+" : "-", RuleName[rule_set][fl], SubRuleName[rule_set][fl], 
+					cantus_id + 1, s + 1, (step0 + fn + s + 1) / 8 + 1, (step0 + fn + s + 1) % 8, midi_file);
 				WriteLog(5, est);
 				false_pos[fl] = 1;
 				// Collect global false positives statistics
