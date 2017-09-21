70a5919872f5c7f54e9b03875fca9426e87c2e09
Rualark <rualark@gmail.com>
2017-09-21 20:19:01 +0300

    CF1:Migrate false positives and emulator parameters to rules.xlsm

--- MGen/Generators/GenCF1.cpp
+++ MGen/Generators/GenCF1.cpp
@@ -410,10 +410,10 @@ int CGenCF1::SelectRuleSet(int rs)
 
 void CGenCF1::LoadConfigLine(CString* sN, CString* sV, int idata, float fdata)
 {
-	LoadVectorPar2(sN, sV, "sas_emulator_delay_ignore", sas_emulator_max_delay, 1000000, 1);
-	LoadVectorPar2(sN, sV, "sas_emulator_move_ignore", sas_emulator_move_ignore, 1, 1);
-	LoadVectorPar2(sN, sV, "false_positives_ignore", false_positives_ignore, 1, 1);
-	LoadVectorPar2(sN, sV, "false_positives_global", false_positives_global, 1, 1);
+	//LoadVectorPar2(sN, sV, "sas_emulator_delay_ignore", sas_emulator_max_delay, 1000000, 1);
+	//LoadVectorPar2(sN, sV, "sas_emulator_move_ignore", sas_emulator_move_ignore, 1, 1);
+	//LoadVectorPar2(sN, sV, "false_positives_ignore", false_positives_ignore, 1, 1);
+	//LoadVectorPar2(sN, sV, "false_positives_global", false_positives_global, 1, 1);
 	CheckVar(sN, sV, "show_ignored_flags", &show_ignored_flags, 0, 2);
 	CheckVar(sN, sV, "emulate_sas", &emulate_sas, 0, 2);
 	CheckVar(sN, sV, "max_correct_ms", &max_correct_ms, 0);
