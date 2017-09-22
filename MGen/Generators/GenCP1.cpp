1b9caee23405697d84a1a78162bbd54d166774f3
Rualark <rualark@gmail.com>
2017-09-22 23:50:52 +0300

    CP1: Always flag long dissonance

--- MGen/Generators/GenCP1.cpp
+++ MGen/Generators/GenCP1.cpp
@@ -564,11 +564,11 @@ int CGenCP1::FailDis() {
 		else if (rpos[ls] == pLeap) FLAG2(187, s)
 		else {
 			FLAG2(169, s)
-			// Check if discord is longer than neighboring consonance
-			if (ls > 0 && llen[ls] > llen[ls - 1] && tivl[fli2[ls - 1]] != iDis) FLAG2(223, s)
-			else if (ls < fli_size - 2 && llen[ls] > llen[ls + 1] && tivl[fli[ls + 1]] != iDis) 
-				FLAG2(223, s);
 		}
+		// Check if discord is longer than neighboring consonance
+		if (ls > 0 && llen[ls] > llen[ls - 1] && tivl[fli2[ls - 1]] != iDis) FLAG2(223, s)
+		else if (ls < fli_size - 2 && llen[ls] > llen[ls + 1] && tivl[fli[ls + 1]] != iDis)
+			FLAG2(223, s);
 	}
 	return 0;
 }
