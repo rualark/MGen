8f4d9986bf01516cc98feb86ba0959f8558fd0a9
Rualark <rualark@gmail.com>
2017-09-26 13:34:03 +0300

    CA2: Add new autotest midi files

--- MGen/configs/GenCA2/test-cp3.pl
+++ MGen/configs/GenCA2/test-cp3.pl
@@ -1,6 +1,6 @@
 include "include/CA2.pl"
 
 # Analysis
-Midi_file = "midi\GenCA2\test-vint.mid" # Path and file name of imported midi file
+Midi_file = "midi\GenCA2\test-cp3.mid" # Path and file name of imported midi file
 
 corrections = 1 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
