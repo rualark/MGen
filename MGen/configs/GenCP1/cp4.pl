43a873038cc322af95ead7819f8837f5a89fc398
Rualark <rualark@gmail.com>
2017-09-26 21:27:07 +0300

    CP1: Autotest generation of all species with ASWA for SAS emulator

---
+++ MGen/configs/GenCP1/cp4.pl
@@ -0,0 +1,10 @@
+include "include/CP1.pl"
+
+# Analysis
+Midi_file = midi\good-cf-min.mid # Path and file name of imported midi file
+#Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
+
+method = swa
+cantus_id = 0
+cantus_high = 1
+species = 4
