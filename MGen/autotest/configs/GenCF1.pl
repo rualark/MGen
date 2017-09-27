dd0f22f7b284d28911a0a0ecc1ac30e9136f9ed1
Rualark <rualark@gmail.com>
2017-09-27 09:56:02 +0300

    MGenTest: Add build type and timestamp to artifact file names (closed #1206)

---
+++ MGen/autotest/configs/GenCF1.pl
@@ -0,0 +1,2 @@
+animate = 100 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
+animate_delay = 0 # Delay in ms after each animation step
--- MGen/autotest/configs/GenCP1.pl
+++ MGen/autotest/configs/GenCP1.pl
@@ -1,2 +1,4 @@
 cantus_id = 0 # Select particular cantus id. Set to 0 to select random
 accept_cantus_rechoose = 1 # Choose new random cantus after accepting counterpoint
+animate = 100 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
+animate_delay = 0 # Delay in ms after each animation step
