bf04b2b0d0e01b12e0e2e4728445b5af02d39284
Rualark <rualark@gmail.com>
2017-09-28 12:26:43 +0300

    CF1: Check if every rule in rule list exists.

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,9 +1,9 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 101 # Id of the currently selected algorithm
-Config = swa # Name of configuration file for selected algorithm
-MIDI_OUT = Microsoft GS Wavetable Synth 0 # Name of MIDI device used for playing notes
+Algorithm = 112 # Id of the currently selected algorithm
+Config = test-tonic-seq # Name of configuration file for selected algorithm
+MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
 Horizontal_zoom = 200 # Zoom of the piano roll. Can be from 1 to 500
 playback_speed = 100 # Playback speed in percent
 show_marks = 1 # Set to 1 to show note mark strings
