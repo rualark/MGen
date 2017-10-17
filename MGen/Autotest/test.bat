61f9537a6e4e233ff31a1c54ab9460051d4f6a83
Rualark <rualark@gmail.com>
2017-10-17 08:23:10 +0300

    MGenTest: Fix autotest.bat for x86

--- MGen/Autotest/test.bat
+++ MGen/Autotest/test.bat
@@ -1,4 +1,8 @@
 cd ..
+copy "..\Debug\MGenTest.exe" .
+copy "..\Debug\MGen.exe" .
+copy "..\Release\MGenTest.exe" .
+copy "..\Release\MGen.exe" .
 copy "..\x64\Debug\MGenTest.exe" .
 copy "..\x64\Debug\MGen.exe" .
 copy "..\x64\Release\MGenTest.exe" .
