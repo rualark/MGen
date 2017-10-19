ac84f8366ac89c6832d8a1651f1c649d716ee22d
Rualark <rualark@gmail.com>
2017-10-19 20:22:41 +0300

    CA5: Fix Last note (1/4) flag

--- MGen/Generators/GenCP1.cpp
+++ MGen/Generators/GenCP1.cpp
@@ -1296,8 +1296,8 @@ int CGenCP1::FailRhythm5() {
 			// Last note
 			if (ep2 == c_len && ls2 == fli_size - 1) {
 				// Check length
-				if (l_len[lp] == 1) FLAG2(253, s2)
-				else if (l_len[lp] == 2) FLAG2(252, s2)
+				if (llen[ls2] == 1) FLAG2(253, s2)
+				else if (llen[ls2] == 2) FLAG2(252, s2)
 			}
 			// Calculate rhythm id
 			if (lp < l_len.size() - 1 || !slur2)
