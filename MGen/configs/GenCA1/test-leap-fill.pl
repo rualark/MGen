f2e1ebf1b650a3d43c60e530b77f836153418009
Rualark <rualark@gmail.com>
2017-10-20 14:42:23 +0300

    Fix expected confirmations

--- MGen/configs/GenCA1/test-leap-fill.pl
+++ MGen/configs/GenCA1/test-leap-fill.pl
@@ -4,3 +4,4 @@ include "include/CA1.pl"
 Midi_file = midi\GenCA1\test-leap-fill.mid # Path and file name of imported midi file
 
 corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
+cantus_high = 1
