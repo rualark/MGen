96a16be5a19f485b3cb50a0b756e9babdb84c2d3
Rualark <rualark@gmail.com>
2017-10-20 14:42:23 +0300

    Fix expected confirmations

--- MGen/configs/GenCA1/test-culm.pl
+++ MGen/configs/GenCA1/test-culm.pl
@@ -3,4 +3,5 @@ include "include/CA1.pl"
 # Analysis
 Midi_file = midi\GenCA1\test-culm.mid
 
+cantus_high = 1
 corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
