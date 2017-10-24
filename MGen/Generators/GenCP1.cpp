be4992fb7a7f67b4202eb342f25ceebcff1e8ad0
Rualark <rualark@gmail.com>
2017-10-24 09:54:52 +0300

    CA5: Anticipation must be harmonic (closed #1392)

--- MGen/Generators/GenCP1.cpp
+++ MGen/Generators/GenCP1.cpp
@@ -252,10 +252,10 @@ void CGenCP1::ScanCPInit() {
 }
 
 void CGenCP1::SendRpos(int pos, int i, int v, int av, int x) {
-	if (rpos[bli[x]] < 0) lining[pos + i][v] = HatchStyleLargeConfetti;
-	else lining[pos + i][v] = 0;
-	//if (tivl[x] == iDis) lining[pos + i][v] = HatchStyleNarrowHorizontal;
+	//if (rpos[bli[x]] < 0) lining[pos + i][v] = HatchStyleLargeConfetti;
 	//else lining[pos + i][v] = 0;
+	if (tivl[x] == iDis) lining[pos + i][v] = HatchStyleNarrowHorizontal;
+	else lining[pos + i][v] = 0;
 }
 
 // Calculate parameter map
