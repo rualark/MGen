4fcaba56f93053c87f5a5ce78a00d99f30cdc25c
Rualark <rualark@gmail.com>
2017-11-14 19:22:54 +0300

    CA2: if cannot correct in SAS, tell user that could not correct (closed #1517)

--- MGen/configs/GenCP1/cp1.pl
+++ MGen/configs/GenCP1/cp1.pl
@@ -1,7 +1,8 @@
 include "include/CP1.pl"
 
 # Analysis
-Midi_file = midi\good-cf-min.mid # Path and file name of imported midi file
+#Midi_file = midi\good-cf-min.mid # Path and file name of imported midi file
+Midi_file = midi\bad-cf.mid # Path and file name of imported midi file
 #Midi_file = "midi\cpointer-2c1.mid" # Path and file name of imported midi file
 
 method = swa
