1c4cfcca2ec1db901bf4a097f6c47921d69af27e
Rualark <rualark@gmail.com>
2017-12-06 01:30:49 +0300

    CF1/CP1: Migrate all macros to semicolon-required

---
+++ MGen/configs/GenCA2/autoconf-cp5-2017-12-02 16-50-50.pl
@@ -0,0 +1,11 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM TEMPLATE CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change template config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Template configs are not accessible from MGen GUI, you will need to change it outside of the program
+# Template config was created by removing Midi_file parameter from source config
+
+include "include/CA2.pl"
+
+# This config was created from default config file configs\GenCA2.pl
+# Created at 2017-12-06 00-56-18
+Midi_file = saves\GenCP1\cp5-2017-12-02 16-50-50\cp5-2017-12-02 16-50-50.mid
