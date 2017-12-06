305e0ebc5d68b8e66a58d26f36cccc5f1b13f216
Rualark <rualark@gmail.com>
2017-12-06 13:03:44 +0300

    CP1: Check LT 8va only for first sus position

--- MGen/configs/GenCP1/good-cf-maj.pl
+++ MGen/configs/GenCP1/good-cf-maj.pl
@@ -5,6 +5,6 @@ Midi_file = midi\good-cf-maj.mid # Path and file name of imported midi file
 
 method = swa
 
-cantus_id = 1 # Select particular cantus id. Set to 0 to select random
+cantus_id = 0 # Select particular cantus id. Set to 0 to select random
 
 cantus_high = 1 # Set to 1 to consider cantus to be higher voice
