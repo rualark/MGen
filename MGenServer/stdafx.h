fb6c1c1d10d2a12f529d63bcf86741cd725fab67
Rualark <rualark@gmail.com>
2017-12-08 19:03:55 +0300

    MGenServer: Restart processes, send status over ODBC

--- MGenServer/stdafx.h
+++ MGenServer/stdafx.h
@@ -26,6 +26,8 @@
 #include <afxcmn.h>                     // MFC support for Windows Common Controls
 #endif // _AFX_NO_AFXCMN_SUPPORT
 
+#include "afxdb.h"
+
 #include <iostream>
 
 #include <cctype>
