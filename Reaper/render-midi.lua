a93ba9f53998bad4b73f913b1cf0682e8397bc6f
Rualark <rualark@gmail.com>
2017-12-08 10:50:03 +0300

    Add reaper integration scripts

---
+++ Reaper/render-midi.lua
@@ -0,0 +1,22 @@
+-- Go to start of project
+reaper.Main_OnCommand(40042, 0);
+-- Deselect all tracks
+reaper.Main_OnCommand(40297, 0);
+-- Select track 1
+reaper.Main_OnCommand(40939, 0);
+-- reaper.SetTrackSelected(reaper.GetTrack(0, 0), 1);
+
+reaper.InsertMedia("C:\\ReaperBuf\\input.mid", 0);
+-- Select all tracks
+reaper.Main_OnCommand(40296, 0);
+-- Render with last settings
+reaper.Main_OnCommand(41824, 0)
+
+-- Select all items
+reaper.Main_OnCommand(40182, 0);
+-- Remove items
+reaper.Main_OnCommand(40006, 0);
+-- Remove all tempo markers
+count_tempo_markers = reaper.CountTempoTimeSigMarkers(0)
+for i=count_tempo_markers,0,-1 do reaper.DeleteTempoTimeSigMarker(0, i) end
+
