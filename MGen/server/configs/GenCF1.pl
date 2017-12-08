8ae4527dbdd2fedd08f9f38f6861809b89e5263d
Rualark <rualark@gmail.com>
2017-12-08 22:26:02 +0300

    MGenServer: Introduce new m_testing=2 mode for job (separate configs, no test logs)

---
+++ MGen/server/configs/GenCF1.pl
@@ -0,0 +1,3 @@
+sas_emu = 0
+animate = 0 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
+animate_delay = 0 # Delay in ms after each animation step
---
+++ MGen/server/configs/GenCP1.pl
@@ -0,0 +1,4 @@
+sas_emu = 0
+cor_ack = 0 # Acknowledge correction results by running both algorithms: SAS and ASWA
+animate = 0 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
+animate_delay = 0 # Delay in ms after each animation step
