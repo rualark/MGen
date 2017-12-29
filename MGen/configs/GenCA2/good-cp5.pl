6317f9943e7c281480d59d2fc0c31b280cd4b9b4
Rualark <rualark@gmail.com>
2017-12-29 23:24:52 +0300

    CF1: Add new confirmation mode for known pieces (closed #1677)

--- MGen/configs/GenCA2/good-cp5.pl
+++ MGen/configs/GenCA2/good-cp5.pl
@@ -4,3 +4,4 @@ include "include/CA2.pl"
 Midi_file = "midi\good-cp5.mid" # Path and file name of imported midi file
 
 corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
+confirm_mode = 2 # 0 - do not confirm expected flags; 1 - confirm global/local; 2 - confirm mistakes
