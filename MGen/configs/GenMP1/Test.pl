d05ab798e9813b6d4599b1c6580ac2d49926b219
Rualark <rualark@gmail.com>
2018-01-13 10:45:18 +0300

    MP1: Add staccato support to Embertone strings

--- MGen/configs/GenMP1/Test.pl
+++ MGen/configs/GenMP1/Test.pl
@@ -1,7 +1,8 @@
 Instruments = Violin,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano,Piano
 
 #Midi_file = "midi\not-midi.mid" # Path and file name of imported midi file
-Midi_file = "midi\GenMP1\test-legato.mid" # Path and file name of imported midi file
+#Midi_file = "midi\GenMP1\test-legato.mid" # Path and file name of imported midi file
+Midi_file = "midi\GenMP1\test-stac.mid" # Path and file name of imported midi file
 
 adapt_enable = 1 # 1 if you want to adapt voices to instruments
 midifile_in_mul = 8 # How much you want to decrease minimum step length from 1/8
