7b723cc91536485541d544585e9c68536998e9f4
Rualark <rualark@gmail.com>
2018-01-14 18:42:00 +0300

    MP1: Upgrade clarinet sound

--- MGen/instruments/Clarinet/SM Clarinet.pl
+++ MGen/instruments/Clarinet/SM Clarinet.pl
@@ -1,19 +1,18 @@
 include "..\include\SM Woodwinds.pl"
 
 # Main
-library = "Samplemodeling Flute 2.8.0" # For which library algorithm is optimized
+library = "Samplemodeling Clarinet 2.8.0" # For which library algorithm is optimized
 
 # Instrument parameters
-n_min = "A#3" # Lowest note
-n_max = "C#7" # Highest note
+n_min = "C#3" # Lowest note
+n_max = "A#6" # Highest note
 t_min = 20 # Shortest note in ms
 t_max = 8000 # Longest note without pauses in ms (0 = no limit). Decreases with dynamics
 #leap_t_min = 100 # Shortest note after leap in ms
 
 # Transition types
-vel_harsh = 50 # Velocity equal or above this value triggers harsh sustain
-vel_immediate = 33 # Velocity equal or above this value triggers immediate sustain
-vel_normal = 10 # Velocity equal or above this value triggers normal sustain
+vel_harsh = 100 # Velocity equal or above this value triggers harsh sustain
+vel_immediate = 50 # Velocity equal or above this value triggers immediate sustain
+vel_normal = 41 # Velocity equal or above this value triggers normal sustain
 vel_normal_minlen = 400 # Minimum note length (ms) that can have a normal or lower sustain
 harsh_freq = 10 # Frequency of harsh sustain articulation in percent of all possible situations
-
--- MGen/instruments/Piccolo/SM Piccolo.pl
+++ MGen/instruments/Piccolo/SM Piccolo.pl
@@ -4,5 +4,6 @@ include "..\Flute\SM Flute.pl"
 library = "Samplemodeling Piccolo 2.8.0" # For which library algorithm is optimized
 
 # Instrument parameters
-n_min = "B3" # Lowest note
-n_max = "C7" # Highest note
+n_min = "B4" # Lowest note
+n_max = "C8" # Highest note
+
