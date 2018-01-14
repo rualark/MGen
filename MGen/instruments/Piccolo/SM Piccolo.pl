4301191ee6068fae74b6e0ebf8a4df09a5ec7861
Rualark <rualark@gmail.com>
2018-01-14 13:01:38 +0300

    MP1: Create configs for all flutes

---
+++ MGen/instruments/Piccolo/SM Piccolo.pl
@@ -0,0 +1,8 @@
+include "..\Flute\SM Flute.pl"
+
+# Main
+library = "Samplemodeling Piccolo 2.8.0" # For which library algorithm is optimized
+
+# Instrument parameters
+n_min = "B3" # Lowest note
+n_max = "C7" # Highest note
