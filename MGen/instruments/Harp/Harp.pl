3865d9bf15bf53d2c988169e14b0ad481605f844
Rualark <rualark@gmail.com>
2018-01-15 09:22:02 +0300

    MP1: Add harp

---
+++ MGen/instruments/Harp/Harp.pl
@@ -0,0 +1,9 @@
+# Main
+library = "ProjectSAM: Concert Harp Exp" # For which library algorithm is optimized
+Type = 0 # Instrument type
+poly = 100 # Maximum number of simultaneous voices
+
+# Instrument parameters
+n_min = "C#1" # Lowest note
+n_max = "F#7" # Highest note
+Volume_default = 127 # (direct CC volume, should not be changed by user)
