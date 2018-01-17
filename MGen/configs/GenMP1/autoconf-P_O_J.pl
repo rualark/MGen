59c9abb5a5d52d9afaadb667e938cc166b6eba61
Rualark <rualark@gmail.com>
2018-01-17 20:22:01 +0300

    MP1: New autoconf for POJ

---
+++ MGen/configs/GenMP1/autoconf-P_O_J.pl
@@ -0,0 +1,13 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM DEFAULT CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change default config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Default configs are not accessible from MGen GUI, you will need to change it outside of the program
+
+adapt_enable = 1 # 1 if you want to adapt voices to instruments
+midifile_in_mul = 4 # How much you want to decrease minimum step length
+
+# This config was created from default config file configs\GenMP1.pl
+# Created at 2018-01-17 20:02:44
+Midi_file = midi\Music\QM\P_O_J.mid
+
+Instruments = "Flute/Normal,Flute/Normal,Oboe/Normal,Oboe/Normal,Clarinet/Normal,Clarinet/Normal,Bassoon/Normal,Bassoon/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Trumpet/Normal,Trumpet/Normal,Trumpet/Normal,Trombone/Normal,Trombone/Normal,Trombone,Tuba/Normal,Timpani/Long hits,Triangle/Hit,Glockenspiel,Harp,Violins/Arco,Violins/Arco,Violas/Arco,Cellos/Arco,Contrabasses/Arco"
