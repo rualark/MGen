ee2647614ad05ceca86fdc4f4b190bc29d9cc294
Rualark <rualark@gmail.com>
2018-01-20 00:14:09 +0300

    MP1: Increase pizzicato level

--- MGen/instruments/Violas/002.Pizzicato.pl
+++ MGen/instruments/Violas/002.Pizzicato.pl
@@ -3,3 +3,4 @@ include "001.Arco.pl"
 Type = 0 # Instrument type
 poly = 100
 InitTechnique = "pizz"
+vel_mul = 70 # Multiply incoming velocity of all midi notes by this percent (100 - do not change)
