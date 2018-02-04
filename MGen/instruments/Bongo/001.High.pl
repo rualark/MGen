649fd4308471c7fdc1b3303dbb91b917524001bc
Rualark <rualark@gmail.com>
2018-02-04 22:38:17 +0300

    MP1: Fix bongo

--- MGen/instruments/Bongo/001.High.pl
+++ MGen/instruments/Bongo/001.High.pl
@@ -1,6 +1,6 @@
 include "All sounds.pl"
 
-ReplacePitch = "E5" # Replace all notes in track with this pitch
+ReplacePitch = "E4" # Replace all notes in track with this pitch
 n_min = 0 # Lowest note
 n_max = 127 # Highest note
 
@@ -8,4 +8,4 @@ n_max = 127 # Highest note
 trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
 trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
 trem_dynamics = 100 # Percent of tremolo dynamics compared to sustain notes
-trem_replace = "A5" # Replace tremolo notes with this note
+trem_replace = "A4" # Replace tremolo notes with this note
