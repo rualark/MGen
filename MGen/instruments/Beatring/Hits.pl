03dc707cb7b3586e5faf21584bc6c9c72e073d0f
Rualark <rualark@gmail.com>
2018-02-06 23:30:52 +0300

    MP1: Implement single_stage

--- MGen/instruments/Beatring/Hits.pl
+++ MGen/instruments/Beatring/Hits.pl
@@ -2,6 +2,7 @@
 library = "True Strike Cinematic Orchestral Percussion" # For which library algorithm is optimized
 Type = 0 # Instrument type
 poly = 100 # Maximum number of simultaneous voices
+single_stage = 1 # Collapse all tracks for this instrument into a single stage
 
 # Instrument parameters
 ReplacePitch = "C7" # Replace all notes in track with this pitch
@@ -12,3 +13,4 @@ trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
 trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
 trem_dynamics = 100 # Percent of tremolo dynamics compared to sustain notes
 trem_replace = "E7" # Replace tremolo notes with this note
+
--- MGen/instruments/Sleighbells/Hits.pl
+++ MGen/instruments/Sleighbells/Hits.pl
@@ -2,6 +2,7 @@
 library = "True Strike Cinematic Orchestral Percussion" # For which library algorithm is optimized
 Type = 0 # Instrument type
 poly = 100 # Maximum number of simultaneous voices
+single_stage = 1 # Collapse all tracks for this instrument into a single stage
 
 # Instrument parameters
 ReplacePitch = "G5" # Replace all notes in track with this pitch
