985ef87be48aa73bef3867a7cb3fd1078c49cf3d
Rualark <rualark@gmail.com>
2018-02-17 13:51:15 +0300

    MP1: Too accented nonlegato between legato transitions in You go to my head sax #1925

--- MGen/instruments/Alto sax/001.Normal.pl
+++ MGen/instruments/Alto sax/001.Normal.pl
@@ -11,7 +11,7 @@ t_max = 8000 # Longest note without pauses in ms (0 = no limit). Decreases with 
 #leap_t_min = 100 # Shortest note after leap in ms
 
 # Transition types
-vel_harsh = 87 # Velocity equal or above this value triggers harsh sustain
+vel_harsh = 50 # Velocity equal or above this value triggers harsh sustain
 vel_immediate = 46 # Velocity equal or above this value triggers immediate sustain
 vel_normal = 40 # Velocity equal or above this value triggers normal sustain
 vel_normal_minlen = 1000 # Minimum note length (ms) that can have a normal or lower sustain
