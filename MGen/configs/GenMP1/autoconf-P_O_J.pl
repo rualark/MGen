3d6a796eb5ed2897b82f7d2f96ad0dd3cfe456c7
Rualark <rualark@gmail.com>
2018-02-18 19:05:40 +0300

    MP1: Harp track name not exported to csv #1801

--- MGen/configs/GenMP1/autoconf-P_O_J.pl
+++ MGen/configs/GenMP1/autoconf-P_O_J.pl
@@ -4,10 +4,11 @@
 # Default configs are not accessible from MGen GUI, you will need to change it outside of the program
 
 adapt_enable = 1 # 1 if you want to adapt voices to instruments
-midifile_in_mul = 4 # How much you want to decrease minimum step length
+midifile_in_mul = 16 # How much you want to decrease minimum step length
 
 # This config was created from default config file configs\GenMP1.pl
 # Created at 2018-01-17 20:02:44
-Midi_file = midi\Music\QM\P_O_J.mid
+Midi_file = midi\Music\Quinn Mason\P_O_J.mid
 
-Instruments = "Flute/Normal,Flute/Normal,Oboe/Normal,Oboe/Normal,Clarinet/Normal,Clarinet/Normal,Bassoon/Normal,Bassoon/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Trumpet/Normal,Trumpet/Normal,Trumpet/Normal,Trombone/Normal,Trombone/Normal,Trombone,Tuba/Normal,Timpani/Long hits,Triangle/Hit,Glockenspiel,Harp,Violins/Arco,Violins/Arco,Violas/Arco,Cellos/Arco,Contrabasses/Arco"
+Instruments = "Flute/Normal,Flute/Normal,Oboe/Normal,Oboe/Normal,Clarinet/Normal,Clarinet/Normal,Bassoon/Normal,Bassoon/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Horn/Normal,Trumpet/Normal,Trumpet/Normal,Trumpet/Normal,Trombone/Normal,Trombone/Normal,Trombone/Normal,Tuba/Normal,Timpani/Long hits,Triangle/Hit,Glockenspiel,Harp,Violin/Arco,Violin/Arco,Viola/Arco,Cello/Arco,Contrabass/Arco"
+MidiFileType = "Sibelius"
