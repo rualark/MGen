80c02c2115f4ea1b5a0b9425afbe1971730fcc52
Rualark <rualark@gmail.com>
2018-02-20 01:22:00 +0300

    MP1: Migrated to preprocessing dyn_range. Removed vel_range

--- MGen/instruments/Violins section/010.Staccato.pl
+++ MGen/instruments/Violins section/010.Staccato.pl
@@ -3,4 +3,4 @@ include "001.Arco.pl"
 Type = 0 # Instrument type
 poly = 100
 InitTechnique = "stac"
-vel_range = 0-60 # Output range of velocity in percent
+dyn_range = 0-60 # Output range of velocity in percent
