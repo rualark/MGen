151021a0e7edcef6d18255e6aa7c41c107914b3c
Rualark <rualark@gmail.com>
2018-02-22 19:26:01 +0300

    Intermediate commit

---
+++ MGen/configs/GenMP1/autoconf-Planned_ending_for_collab_quartet_6-9-17.pl
@@ -0,0 +1,18 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM DEFAULT CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change default config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Default configs are not accessible from MGen GUI, you will need to change it outside of the program
+
+adapt_enable = 1 # 1 if you want to adapt voices to instruments
+midifile_in_mul = 16 # How much you want to decrease minimum step length
+
+# This config was created from default config file configs\GenMP1.pl
+# Created at 2018-02-22 17:45:09
+Midi_file = midi\Music\Quinn Mason\Planned_ending_for_collab_quartet_6-9-17.mid
+
+Instruments = "Violin,Violin,Viola,Cello"
+MidiFileType = "Sibelius"
+"Violin/Arco/1": Pan = "10"
+"Violin/Arco/2": Pan = "31"
+"Viola/Arco/3": Pan = "76"
+"Cello/Arco/4": Pan = "90"
