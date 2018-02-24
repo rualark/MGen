14bdbb748e2c435d5833cef8eb32effe9a0d69f4
Rualark <rualark@gmail.com>
2018-02-24 14:42:32 +0300

    LY: Make flat symbol two times smaller #1961. MP1: Sync MIDI channel number between kontakt, daw and layout config (start from 1) #1782

--- MGen/instruments/Timpani/_Default.pl
+++ MGen/instruments/Timpani/_Default.pl
@@ -18,5 +18,5 @@ CC_steps = 1 # Number of CC steps in one note step (please use only odd numbers)
 trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
 trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
 trem_dyn_range = 0-100 # Percent of tremolo dynamics compared to sustain notes
-trem_chan = 1 # Send tremolo notes to separate channel specified
+trem_chan = 2 # Send tremolo notes to separate channel specified
 
