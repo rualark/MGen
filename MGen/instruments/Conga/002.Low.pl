8ba54a5f6186b1282632c5a07bb1ef1783ac326f
Rualark <rualark@gmail.com>
2018-03-04 20:36:04 +0300

    MP1: Allow to convert trills to tremolo #2010

--- MGen/instruments/Conga/002.Low.pl
+++ MGen/instruments/Conga/002.Low.pl
@@ -7,3 +7,4 @@ trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
 trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
 trem_dyn_range = 0-100 # (0-100) Percent of tremolo dynamics compared to sustain notes
 trem_replace = "A3" # Replace tremolo notes with this note
+trem_maxint = 127 # Maximum interval between notes of a trill in semitones to be considered a tremolo
--- MGen/instruments/Snare ensemble/All sounds.pl
+++ MGen/instruments/Snare ensemble/All sounds.pl
@@ -19,3 +19,4 @@ trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
 trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
 trem_dyn_range = 0-100 # (0-100) Percent of tremolo dynamics compared to sustain notes
 trem_replace = "F4" # Replace tremolo notes with this note
+trem_maxint = 127 # Maximum interval between notes of a trill in semitones to be considered a tremolo
