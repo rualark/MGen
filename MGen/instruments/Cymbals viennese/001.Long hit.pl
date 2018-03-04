a66876774bf8ef474490ea9ef06ba6aa1eea87dc
Rualark <rualark@gmail.com>
2018-03-04 18:13:48 +0300

    MP1: Add cymbals tremolo. Add viennese cymbals

---
+++ MGen/instruments/Cymbals viennese/001.Long hit.pl
@@ -0,0 +1,3 @@
+include "All sounds.pl"
+
+ReplacePitch = "C4" # Replace all notes in track with this pitch
---
+++ MGen/instruments/Cymbals viennese/All sounds.pl
@@ -0,0 +1,16 @@
+# Main
+library = "True Strike Cinematic Orchestral Percussion" # For which library algorithm is optimized
+Type = 0 # Instrument type
+poly = 100 # Maximum number of simultaneous voices
+single_stage = 1 # Collapse all tracks for this instrument into a single stage
+
+# Instrument parameters
+n_min = "C2" # Lowest note
+n_max = "C5" # Highest note
+Volume_default = 50 # (direct CC volume, should not be changed by user)
+
+# Tremolo adaptor
+trem_maxlen = 100 # Maximum length of one note inside tremolo for detection
+trem_min_repeats = 3 # Minimum number of note repeats to detect tremolo
+trem_dyn_range = 0-100 # Percent of tremolo dynamics compared to sustain notes
+trem_replace = "B4" # Replace tremolo notes with this note
