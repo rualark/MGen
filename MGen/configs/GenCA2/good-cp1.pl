bcf8dbd2fb5583781131341133d97a3aa5e9b40f
Rualark <rualark@gmail.com>
2018-03-29 22:14:13 +0300

    CA2: Fix correction acknowledge failure due to flagging 157 at the end of scan window

--- MGen/configs/GenCA2/good-cp1.pl
+++ MGen/configs/GenCA2/good-cp1.pl
@@ -3,4 +3,9 @@ include "include/CA2.pl"
 # Analysis
 Midi_file = "midi\Counterpoint\good\good-cp1.mid" # Path and file name of imported midi file
 
-corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
+corrections = 1 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
+
+max_correct_ms = 2000 # Maximum time in milliseconds to correct using window-scan (set to 0 to scan up to the end)
+cor_ack = 1 # Acknowledge correction results by running both algorithms: SAS and ASWA
+animate = 0 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
+animate_delay = 0 # Delay in ms after each animation step
