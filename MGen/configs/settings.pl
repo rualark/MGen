b7dd8c67e6d2862095160d92ab41b8c3daaef14e
Rualark <rualark@gmail.com>
2018-04-12 14:33:10 +0300

    CA2: Calculate cp_retrig. CA2: Load retrigger for anticipations from midi files to analyse correctly #1664

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -2,7 +2,7 @@
 # This file is loaded on MGen startup and automatically saved on every setting change
 
 Algorithm = 112 # Id of the currently selected algorithm
-Config = autoconf-TP1 # Name of configuration file for selected algorithm
+Config = autoconf-test-retrigger # Name of configuration file for selected algorithm
 MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
 Horizontal_zoom = 200 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
