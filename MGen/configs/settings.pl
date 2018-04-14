af1db4df15611df270b03b2b49f1557c72d8cd3b
Rualark <rualark@gmail.com>
2018-04-14 11:39:55 +0300

    CP1: Fix cantus key detecion

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,9 +1,9 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 112 # Id of the currently selected algorithm
-Config = autoconf-TP4 # Name of configuration file for selected algorithm
-MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
+Algorithm = 121 # Id of the currently selected algorithm
+Config = good-cf-maj-high # Name of configuration file for selected algorithm
+MIDI_OUT = loopMIDI Port 2 # Name of MIDI device used for playing notes
 Horizontal_zoom = 600 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
 show_marks = 1 # Set to 1 to show note mark strings
@@ -15,7 +15,7 @@ show_progress = 1 # Set to 1 to show note scan progress
 show_vel = 0 # Set to 1 to show note velocity using alpha
 show_curve = 0 # Set to 1 to show melody curves (MeloCurve)
 show_nsr = 0 # Set to 1 to show note scan range
-show_tempo = 0 # Set to 1 to show tempo
+show_tempo = 1 # Set to 1 to show tempo
 show_graph0 = 0 # Set to 1 to show graph 0
 show_graph1 = 0 # Set to 1 to show graph 1
 show_graph2 = 0 # Set to 1 to show graph 2
