a7b2a42f2f7f3422a7e5301a8e8ec07eb2cbcc88
Rualark <rualark@gmail.com>
2018-04-23 20:37:57 +0300

    CA2: Migrate to cantus_id

--- MGen/configs/GenCA2/test-long.pl
+++ MGen/configs/GenCA2/test-long.pl
@@ -4,3 +4,4 @@ include "include/CA2.pl"
 Midi_file = "midi\Counterpoint\test\test-long.mid" # Path and file name of imported midi file
 
 corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
+
--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,8 +1,8 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 111 # Id of the currently selected algorithm
-Config = test-fill # Name of configuration file for selected algorithm
+Algorithm = 112 # Id of the currently selected algorithm
+Config = test-long # Name of configuration file for selected algorithm
 MIDI_OUT = loopMIDI Port 2 # Name of MIDI device used for playing notes
 Horizontal_zoom = 600 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
