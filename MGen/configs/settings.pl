4a962cd23b10b1b0395fa74f7ec639f05cc085ea
Rualark <rualark@gmail.com>
2018-04-27 21:43:25 +0300

    Adapt: Tempo randomization slowness now constantly depends on instant tempo

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,10 +1,10 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 112 # Id of the currently selected algorithm
-Config = test-cp5-rhythm # Name of configuration file for selected algorithm
+Algorithm = 2001 # Id of the currently selected algorithm
+Config = test-tempo # Name of configuration file for selected algorithm
 MIDI_OUT = loopMIDI Port 2 # Name of MIDI device used for playing notes
-Horizontal_zoom = 600 # Zoom of the piano roll. Can be from 1 to 2000
+Horizontal_zoom = 10 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
 show_marks = 1 # Set to 1 to show note mark strings
 show_comments = 0 # Set to 1 to surround notes which have comments with rectangles
@@ -16,7 +16,7 @@ ly_debugexpect = 0 # Set to 1 to generate debug expect in lilypond files
 show_vel = 0 # Set to 1 to show note velocity using alpha
 show_curve = 0 # Set to 1 to show melody curves (MeloCurve)
 show_nsr = 0 # Set to 1 to show note scan range
-show_tempo = 0 # Set to 1 to show tempo
+show_tempo = 1 # Set to 1 to show tempo
 show_graph0 = 1 # Set to 1 to show graph 0
 show_graph1 = 0 # Set to 1 to show graph 1
 show_graph2 = 0 # Set to 1 to show graph 2
