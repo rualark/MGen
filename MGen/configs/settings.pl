a28fedc7d27fd11e920e482734fb4919dd29dcc7
Rualark <rualark@gmail.com>
2018-04-28 23:02:36 +0300

    Revert "MP1: Add InterpolateCC debug code"
    This reverts commit 3ea08ce8ee0ea64047e0d01312153bb05238f7f4.

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,10 +1,10 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 112 # Id of the currently selected algorithm
-Config = test-tritone # Name of configuration file for selected algorithm
-MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
-Horizontal_zoom = 600 # Zoom of the piano roll. Can be from 1 to 2000
+Algorithm = 2001 # Id of the currently selected algorithm
+Config = test-tempo # Name of configuration file for selected algorithm
+MIDI_OUT = loopMIDI Port 2 # Name of MIDI device used for playing notes
+Horizontal_zoom = 10 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
 show_marks = 1 # Set to 1 to show note mark strings
 show_comments = 0 # Set to 1 to surround notes which have comments with rectangles
@@ -13,11 +13,11 @@ show_lining = 1 # Set to 1 to show note lining
 show_lines = 1 # Set to 1 to show line separators
 show_progress = 1 # Set to 1 to show note scan progress
 ly_debugexpect = 0 # Set to 1 to generate debug expect in lilypond files
-show_vel = 1 # Set to 1 to show note velocity using alpha
+show_vel = 0 # Set to 1 to show note velocity using alpha
 show_curve = 0 # Set to 1 to show melody curves (MeloCurve)
 show_nsr = 0 # Set to 1 to show note scan range
 show_tempo = 1 # Set to 1 to show tempo
-show_graph0 = 0 # Set to 1 to show graph 0
+show_graph0 = 1 # Set to 1 to show graph 0
 show_graph1 = 0 # Set to 1 to show graph 1
 show_graph2 = 0 # Set to 1 to show graph 2
 show_notecolors = 1 # Set to 1 to show note colors instead of instrument colors
