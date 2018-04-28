df85bb2c8449a25f46107e7afac060a914dc803d
Rualark <rualark@gmail.com>
2018-04-29 00:16:04 +0300

    MP1: Make tempo randomisation slowness controllable same as with dynamics random slowness #2166

--- MGen/configs/GenCA2/test-tritone.pl
+++ MGen/configs/GenCA2/test-tritone.pl
@@ -6,3 +6,6 @@ Midi_file = "midi\Counterpoint\test\test-tritone.mid" # Path and file name of im
 corrections = 0 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
 
 Instruments = Violin
+
+rnd_tempo_slow = 0.3
+rnd_tempo = 100
