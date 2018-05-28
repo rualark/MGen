e4a5c830ac68e6bbb11509c646812f19a6511a05
Rualark <rualark@gmail.com>
2018-05-28 09:47:35 +0300

    Docs: Update. Renamed Галлон-Бик

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -2,7 +2,7 @@
 # This file is loaded on MGen startup and automatically saved on every setting change
 
 Algorithm = 112 # Id of the currently selected algorithm
-Config = test-sus # Name of configuration file for selected algorithm
+Config = TP7 # Name of configuration file for selected algorithm
 MIDI_OUT = loopMIDI Port 2 # Name of MIDI device used for playing notes
 Horizontal_zoom = 600 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
