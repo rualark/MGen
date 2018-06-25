80974e2e8a8348c3f498d79c9f2f12a4791a54cc
Rualark <rualark@gmail.com>
2018-06-25 20:28:57 +0300

    LY: Repeat lyrics names on each system row #2268

--- MGen/configs/GenCF1/include/CF1.pl
+++ MGen/configs/GenCF1/include/CF1.pl
@@ -59,7 +59,7 @@ tempo_bell = 1.3 # Increase tempo in the middle of cantus / counterpoint. 0 - pr
 
 show_ignored_flags = 0 # Show even ignored flags (with strikethrough in rules.xlsm)
 show_allowed_flags = 0 # Show even allowed flags (bold in rules.xlsm)
-show_min_severity = 0 # Minimum severity to highlight note
+show_min_severity = 49 # Minimum severity to highlight note
 
 # Lilypond parameters
 ly_flag_style = 2 # 0 - no flag visualisation, 1 - color note, 2 - x above note
@@ -70,7 +70,7 @@ ly_pagebreak = 0 # Page break after each analysis
 # 0 - Show only rule name up to colon. Show only subrules starting with colon
 # 1 - Add subrules without colon
 # 2 - Add rule comments
-ly_rule_verbose = 2 
+ly_rule_verbose = 0 
 
 midifile_export_marks = 1 # Set to export marks (harmony) to midifile lyrics
 midifile_export_comments = 0 # Set to export comments (violations) to midifile lyrics
