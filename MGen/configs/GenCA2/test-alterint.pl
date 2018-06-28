505988b344f3aec0583f31a11ba44394bde85d1d
Rualark <rualark@gmail.com>
2018-06-28 03:13:40 +0300

    MP1: Change vibrato algorithm

--- MGen/configs/GenCA2/test-alterint.pl
+++ MGen/configs/GenCA2/test-alterint.pl
@@ -4,7 +4,7 @@ include "include/CA2.pl"
 Midi_file = "midi\Counterpoint\test\test-alterint.mid" # Path and file name of imported midi file
 
 corrections = 1 # Number of corrections to show. Set to 0 to disable corrections. Does not work for swa.
-max_correct_ms = 2000 # Maximum time in milliseconds to correct using window-scan (set to 0 to scan up to the end)
+max_correct_ms = 1000 # Maximum time in milliseconds to correct using window-scan (set to 0 to scan up to the end)
 cor_ack = 1 # Acknowledge correction results by running both algorithms: SAS and ASWA
 animate = 0 # Draw animation of preliminary result every X ms (0 to show each change, high to disable animation)
 animate_delay = 0 # Delay in ms after each animation step
