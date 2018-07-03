06bf5b1d1e6b90867c469c0b641066162292c2ef
Rualark <rualark@gmail.com>
2018-07-04 00:20:04 +0300

    MP1: Better default values for vibrato

--- MGen/instruments/include/SI VoR.pl
+++ MGen/instruments/include/SI VoR.pl
@@ -61,14 +61,19 @@ bell_len = 30-30 # Percent of notelength to use for slope at bell start-end
 # Reverse bell adaptor
 rbell_freq = 80 # Frequency to apply reverse bell when all conditions met
 rbell_dur = 300-1000 # Minimum note duration (ms) that can have a reverse bell - that will have deepest reverse bell
+vib_sbell_freq = 100 # Frequency to apply vibrato bell when all conditions met
 rbell_mul = 0.8-0.2 # Multiply dynamics by this parameter at bell center with mindur - with longer dur
 rbell_pos = 20-80 # Leftmost-rightmost minimum reverse bell position inside window (percent of window duration)
 
 # Vibrato adaptor
 CC_vib = 80 # CC number for vibrato intensity
 vib_bell_top = 5-95 # Leftmost-rightmost maximum vibrato intensity in note (percent of note duration)
-vib_bell_exp = 2 # Exponent to create non-linear bell shape
+vib_bell_exp = 2 # Exponent to create non-linear bell shape (left)
+vibf_bell_exp = 2 # Exponent to create non-linear bell shape (left)
+vib_bell_rexp = 2 # Exponent to create non-linear bell shape (right)
+vibf_bell_rexp = 2 # Exponent to create non-linear bell shape (right)
 vib_bell_freq = 100 # Frequency to apply vibrato bell when all conditions met
+vib_sbell_freq = 100 # Frequency to apply vibrato bell when all conditions met
 vib_bell_dur = 600-1200 # Minimum note duration (ms) that can have a vibrato bell - that can have highest vibrato bell
 vib_bell = 50-126 # Maximum vibrato intensity in vibrato bell (for minimum and highest duration)
 rnd_vib = 10 # Randomize vibrato intensity not greater than this percent
