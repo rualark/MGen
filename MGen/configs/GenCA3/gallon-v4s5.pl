2b1b49a87ece8f2b05b4bd4cf7c397a5e7d6d8e8
Rualark <rualark@gmail.com>
2018-07-15 15:31:26 +0300

    CA3: Load npm, mli. Start to implement AnalyseCP

--- MGen/configs/GenCA3/gallon-v4s5.pl
+++ MGen/configs/GenCA3/gallon-v4s5.pl
@@ -4,4 +4,6 @@ include "include/CA3.pl"
 # Created at 2018-07-12 23:37:06
 MusicXML_file = midi\CA3\gallon\gallon-v4s5.xml
 
-Instruments = Piano,Violin,Viola/Sul ponticello
+#Instruments = Piano,Violin,Viola/Sul ponticello
+
+species = 0555
--- MGen/configs/GenCA3/include/CA3.pl
+++ MGen/configs/GenCA3/include/CA3.pl
@@ -1,1 +1,4 @@
 rules_file = rules2\rules.csv # Specify which file contains all rules
+
+lclimax_notes = 13 # Local climax: Highest note in higher voice within 13 notes or 8 measures - which is smaller
+lclimax_mea = 8 # Local climax: Highest note in higher voice within 13 notes or 8 measures - which is smaller
