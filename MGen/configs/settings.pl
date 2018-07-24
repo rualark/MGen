7e761aa540d47bdae8ce5105e33564a1ada7d47c
Rualark <rualark@gmail.com>
2018-07-24 23:45:35 +0300

    CA3: Add FailNoteRepeat

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -1,8 +1,8 @@
 # Settings of MGen
 # This file is loaded on MGen startup and automatically saved on every setting change
 
-Algorithm = 113 # Id of the currently selected algorithm
-Config = good-cp5 # Name of configuration file for selected algorithm
+Algorithm = 111 # Id of the currently selected algorithm
+Config = test-leaps # Name of configuration file for selected algorithm
 MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
 Horizontal_zoom = 300 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
