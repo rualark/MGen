dacf20eedc97bf296b0fa0131f7fd98c1c7040cb
Rualark <rualark@gmail.com>
2018-09-22 11:53:03 +0300

    CA3: Add examples

--- mgen-backup-to-net.bat
+++ mgen-backup-to-net.bat
@@ -2,5 +2,9 @@
 setlocal EnableDelayedExpansion
 set Source=s:\app\MGen
 set Target=n:\public\critic\app\MGen
-robocopy /e /mir "%Source%" "%Target%" * > backup-to-net.log
+rem robocopy /e /mir /sl "%Source%" "%Target%" * /XF "%Source%\MGen\server" /COPY:DATS
+rem > backup-to-net.log
+
+"c:\program files\winrar\winrar" a -r -ma5 -ep1 "%Target%.rar" "%Source%" -x"%Source%\MGen\server" -x"%Source%\MGen\share"
+
 
