a2ce2091ca9fc78bb52a0429157a501aee794e47
Rualark <rualark@gmail.com>
2018-10-28 00:34:54 +0300

    Intermediate commit

---
+++ MGen/configs/GenCA3/good-cp4.pl
@@ -0,0 +1,11 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM TEMPLATE CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change template config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Template configs are not accessible from MGen GUI, you will need to change it outside of the program
+# Template config was created by removing Midi_file parameter from source config
+
+include "include/CA3.pl"
+
+# This config was created from default config file configs\GenCA3.pl
+# Created at 2018-10-27 18:58:59
+MusicXML_file = music\Counterpoint 2 voices\good\good-cp4.xml
