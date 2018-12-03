d7764cc6e4d18a4e13eb21769628a9ccecd9d5ec
Rualark <rualark@gmail.com>
2018-12-03 21:20:35 +0300

    CA3: Implement "suspension sounds with suspension resolution" #2479

--- mgen-backup-to-net.bat
+++ mgen-backup-to-net.bat
@@ -5,6 +5,7 @@ set Target=n:\public\critic\app\MGen
 rem robocopy /e /mir /sl "%Source%" "%Target%" * /XF "%Source%\MGen\server" /COPY:DATS
 rem > backup-to-net.log
 
+del "%Target%.rar"
 "c:\program files\winrar\winrar" a -r -ma5 -ep1 "%Target%.rar" "%Source%" -x"%Source%\MGen\server" -x"%Source%\MGen\share"
 
 
