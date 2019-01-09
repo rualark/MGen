5080c83045e040c35b8137395954bd685786604a
Rualark <rualark@gmail.com>
2019-01-10 00:36:20 +0300

    CA3: Swap chord voices: ReorderChords

---
+++ MGen/configs/GenCA3/known-cp110.pl
@@ -0,0 +1,12 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM TEMPLATE CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change template config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Template configs are not accessible from MGen GUI, you will need to change it outside of the program
+# Template config was created by removing Midi_file parameter from source config
+
+include "include/CA3.pl"
+
+# This config was created from default config file configs\GenCA3.pl
+# Created at 2019-01-10 00:29:01
+MusicXML_file = music\Counterpoint 3 voices\known\known-cp110.xml
+species=1
