b944b3998da952481fc2ca5abdb0758b3b453f47
Rualark <rualark@gmail.com>
2019-01-13 14:44:42 +0300

    CA3: Species should be converted from v_cnt to av_cnt #2530

--- MGen/configs/GenCA3/Erreurs TP 1.pl
+++ MGen/configs/GenCA3/Erreurs TP 1.pl
@@ -5,7 +5,6 @@
 # Template config was created by removing Midi_file parameter from source config
 
 include "include/CA3.pl"
-voices_order_pitch = 3 # Difference in average pitch between adjacent voices to initiate voice swap. Set to 0 to disable swap
 
 # This config was created from default config file configs\GenCA3.pl
 # Created at 2019-01-13 11:10:05
