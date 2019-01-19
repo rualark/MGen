e3f0f45841fc9b31c707ca322b3e9498e0fe9629
Rualark <rualark@gmail.com>
2019-01-19 22:48:02 +0300

    CA3: Fix beat bugs. Introduce 5/4 time signature for species 2 and 4

---
+++ MGen/configs/GenCA3/Erreurs TP 1-mod.pl
@@ -0,0 +1,12 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM TEMPLATE CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change template config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Template configs are not accessible from MGen GUI, you will need to change it outside of the program
+# Template config was created by removing Midi_file parameter from source config
+
+include "include/CA3.pl"
+
+# This config was created from default config file configs\GenCA3.pl
+# Created at 2019-01-19 15:35:16
+MusicXML_file = music\Counterpoint 4 voices\problemes\Erreurs TP 1-mod.xml
+species=1
