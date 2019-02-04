f9db15ce54e36d7744ddaf337e8f72f028707666
Rualark <rualark@gmail.com>
2019-02-04 18:58:17 +0300

    CA3: Use nih, not msh to detect harmonic tritone with bass #2594

--- MGen/configs/settings.pl
+++ MGen/configs/settings.pl
@@ -2,7 +2,7 @@
 # This file is loaded on MGen startup and automatically saved on every setting change
 
 Algorithm = 113 # Id of the currently selected algorithm
-Config = test-tritone3 # Name of configuration file for selected algorithm
+Config = test-tritone-fleuri # Name of configuration file for selected algorithm
 MIDI_OUT = [DISABLE PLAYBACK] # Name of MIDI device used for playing notes
 Horizontal_zoom = 400 # Zoom of the piano roll. Can be from 1 to 2000
 playback_speed = 100 # Playback speed in percent
