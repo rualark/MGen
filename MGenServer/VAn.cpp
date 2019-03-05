6194b84a505fa2fd7c6ca0088e86dd101f6c9055
Rualark <rualark@gmail.com>
2019-03-05 20:06:57 +0300

    MGenServer: Migrate to vector dyn. Fix inf generation.

---
+++ MGenServer/VAn.cpp
@@ -0,0 +1,13 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+#include "VAn.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+VAn::VAn() {
+}
+
+VAn::~VAn() {
+}
---
+++ MGenServer/VAn.h
@@ -0,0 +1,11 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
+
+#pragma once
+
+class VAn {
+public:
+	VAn();
+	~VAn();
+};
+
