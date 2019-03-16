f1b0ab45a6cac3a8992892684f99c2b4b6e1c09f
Rualark <rualark@gmail.com>
2019-03-17 01:41:58 +0300

    MP1: Rename folders

---
+++ MGen/configs/GenMP1/Holst-Jupiter.pl
@@ -0,0 +1,16 @@
+# THIS CONFIG WAS AUTOMATICALLY CREATED FROM DEFAULT CONFIG FILE TO OPEN A MIDI FILE
+# YOU CAN EDIT THIS CONFIG FILE TO OPEN THIS FILE AGAIN
+# If you want to change default config file, that is copied each time you open a MIDI file, please go to configs folder in your File browser
+# Default configs are not accessible from MGen GUI, you will need to change it outside of the program
+
+adapt_enable = 1 # 1 if you want to adapt voices to instruments
+midifile_in_mul = 16 # How much you want to decrease minimum step length
+AutoLegato = 0
+
+# This config was created from default config file configs\GenMP1.pl
+# Created at 2019-03-16 16:21:05
+Midi_file = music\Music\Holst\Holst-Jupiter.mid
+
+MidiFileType = "MuseScore" # Injected by Alexey Arkhipenko at 2019-03-14 23:35:03
+toload_time = "25" # Injected by Alexey Arkhipenko at 2019-03-14 23:36:03
+Instruments = "Piccolo,Flute,Oboe,English Horn,Clarinet,Clarinet,Bassoon,Bassoon,Horn,Horn,Horn,Trumpet,Trumpet,Trombone,Trombone,Tuba,Tuba,Timpani,Timpani,Piano,Piano,Piano,Piano,Glockenspiel,Harp,Harp,Violins section,Violins II section,Violas section,Cellos section,Contrabasses section" # Injected by Alexey Arkhipenko at 2019-03-14 23:38:03
